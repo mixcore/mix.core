84f352c639575f62d7228c80df0bed9881ae9382
hoang <nhathoang989@gmail.com>
2018-09-30 15:07:50 +0700

    upd

---
+++ src/Mix.Cms.Api/Controllers/v1/ApiModuleController.cs
@@ -0,0 +1,193 @@
+﻿// Licensed to the Mix I/O Foundation under one or more agreements.
+// The Mix I/O Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Mix.Domain.Core.ViewModels;
+using Mix.Cms.Lib.Models.Cms;
+using Mix.Cms.Lib;
+using Mix.Cms.Lib.Services;
+using static Mix.Cms.Lib.MixEnums;
+using System.Linq.Expressions;
+using System.Web;
+using Mix.Cms.Lib.ViewModels.MixModules;
+
+namespace Mix.Cms.Api.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/v1/{culture}/module")]
+    public class ApiModuleController :
+        BaseApiController
+    {
+        public ApiModuleController()
+        {
+        }
+        #region Get
+
+        // GET api/category/id
+        [HttpGet, HttpOptions]
+        [Route("delete/{id}")]
+        public async Task<RepositoryResponse<MixModule>> DeleteAsync(int id)
+        {
+            var getModule = await ReadListItemViewModel.Repository.GetSingleModelAsync(
+                model => model.Id == id && model.Specificulture == _lang);
+            if (getModule.IsSucceed)
+            {
+
+                return await getModule.Data.RemoveModelAsync(true);
+            }
+            else
+            {
+                return new RepositoryResponse<MixModule>()
+                {
+                    IsSucceed = false
+                };
+            }
+        }
+
+        // GET api/pages/id
+        [HttpGet, HttpOptions]
+        [Route("details/{id}/{viewType}")]
+        [Route("details/{viewType}")]
+        public async Task<ActionResult<JObject>> Details(string viewType, int? id)
+        {
+            switch (viewType)
+            {
+                case "portal":
+                    if (id.HasValue)
+                    {
+                        var beResult = await UpdateViewModel.Repository.GetSingleModelAsync(model => model.Id == id && model.Specificulture == _lang).ConfigureAwait(false);
+                        return Ok(JObject.FromObject(beResult));
+                    }
+                    else
+                    {
+                        var model = new MixModule()
+                        {
+                            Specificulture = _lang,
+                            Status = MixService.GetConfig<int>(MixConstants.ConfigurationKeyword.DefaultContentStatus),
+                            PageSize = 20
+                            ,
+                            Priority = UpdateViewModel.Repository.Max(a => a.Priority).Data + 1
+                        };
+
+                        RepositoryResponse<UpdateViewModel> result = new RepositoryResponse<UpdateViewModel>()
+                        {
+                            IsSucceed = true,
+                            Data = await UpdateViewModel.InitViewAsync(model)
+                        };
+                        return JObject.FromObject(result);
+                    }
+                default:
+                    if (id.HasValue)
+                    {
+                        var beResult = await ReadMvcViewModel.Repository.GetSingleModelAsync(model => model.Id == id && model.Specificulture == _lang).ConfigureAwait(false);
+                        return JObject.FromObject(beResult);
+                    }
+                    else
+                    {
+                        var model = new MixModule();
+                        RepositoryResponse<ReadMvcViewModel> result = new RepositoryResponse<ReadMvcViewModel>()
+                        {
+                            IsSucceed = true,
+                            Data = new ReadMvcViewModel(model)
+                            {
+                                Specificulture = _lang,
+                                Status = MixContentStatus.Preview,
+                                PageSize = 20
+                            }
+                        };
+                        return JObject.FromObject(result);
+                    }
+            }
+        }
+
+
+        #endregion Get
+
+        #region Post
+
+        // POST api/category
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = "SuperAdmin, Admin")]
+        [HttpPost, HttpOptions]
+        [Route("save")]
+        public async Task<RepositoryResponse<UpdateViewModel>> Post([FromBody]UpdateViewModel model)
+        {
+            if (model != null)
+            {
+                model.CreatedBy = User.Claims.FirstOrDefault(c => c.Type == "Username")?.Value;
+                var result = await model.SaveModelAsync(true).ConfigureAwait(false);
+                return result;
+            }
+            return new RepositoryResponse<UpdateViewModel>() { Status = 501 };
+        }
+
+        // POST api/category
+        [HttpPost, HttpOptions]
+        [Route("save/{id}")]
+        public async Task<RepositoryResponse<MixModule>> SaveFields(int id, [FromBody]List<EntityField> fields)
+        {
+            if (fields != null)
+            {
+                var result = new RepositoryResponse<MixModule>() { IsSucceed = true };
+                foreach (var property in fields)
+                {
+                    if (result.IsSucceed)
+                    {
+                        result = await ReadListItemViewModel.Repository.UpdateFieldsAsync(c => c.Id == id && c.Specificulture == _lang, fields).ConfigureAwait(false);
+                    }
+                    else
+                    {
+                        break;
+                    }
+
+                }
+                return result;
+            }
+            return new RepositoryResponse<MixModule>();
+        }
+
+        // GET api/category
+        [HttpPost, HttpOptions]
+        [Route("list")]
+        public async Task<JObject> GetList(
+            [FromBody] RequestPaging request)
+        {
+
+            ParseRequestPagingDate(request);
+            Expression<Func<MixModule, bool>> predicate = model =>
+                        model.Specificulture == _lang
+                        && (!request.Status.HasValue || model.Status == request.Status.Value)
+                        && (string.IsNullOrWhiteSpace(request.Keyword)
+                            || (model.Title.Contains(request.Keyword)
+                            || model.Description.Contains(request.Keyword)))
+                        && (!request.FromDate.HasValue
+                            || (model.CreatedDateTime >= request.FromDate.Value)
+                        )
+                        && (!request.ToDate.HasValue
+                            || (model.CreatedDateTime <= request.ToDate.Value)
+                        );
+
+            switch (request.Key)
+            {
+                case "mvc":
+                    var mvcResult = await ReadMvcViewModel.Repository.GetModelListByAsync(predicate, request.OrderBy, request.Direction, request.PageSize, request.PageIndex).ConfigureAwait(false);
+                    return JObject.FromObject(mvcResult);
+                case "portal":
+                    var portalResult = await UpdateViewModel.Repository.GetModelListByAsync(predicate, request.OrderBy, request.Direction, request.PageSize, request.PageIndex).ConfigureAwait(false);
+                    return JObject.FromObject(portalResult);
+                default:
+                    var data = await ReadListItemViewModel.Repository.GetModelListByAsync(predicate, request.OrderBy, request.Direction, request.PageSize, request.PageIndex).ConfigureAwait(false);
+                    return JObject.FromObject(data);
+            }
+        }
+
+        #endregion Post
+    }
+}
