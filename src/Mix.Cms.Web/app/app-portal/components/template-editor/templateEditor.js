aa6d192a2062894c6208c3ead5ad12db51cd7504
hoang <nhathoang989@gmail.com>
2018-10-27 16:43:43 +0700

    fix add new template on details page

--- src/Mix.Cms.Web/app/app-portal/components/template-editor/templateEditor.js
+++ src/Mix.Cms.Web/app/app-portal/components/template-editor/templateEditor.js
@@ -9,9 +9,26 @@
                 ctrl.template = template;
                 $scope.$broadcast('updateContentCodeEditors', []);
             };
+            ctrl.init = async function(){
+                if(ctrl.folderType){
+                    var themeId = globalSettingsService.get('themeId');
+                    var resp = await service.getSingle(['portal', themeId, ctrl.folderType]);
+                    if (resp && resp.isSucceed) {
+                        resp.data.fileName = 'new';
+                        ctrl.templates.splice(0,0,resp.data);
+                        $rootScope.isBusy = false;
+                        $scope.$apply();
+                    }
+                    else {
+                        if (resp) { $rootScope.showErrors(resp.errors);$rootScope.isBusy = false;$scope.$apply(); }
+                    }
+                    
+                }
+            }
         }],
     bindings: {
         template: '=',
-        templates: '='
+        templates: '=',
+        folderType: '='
     }
 });
