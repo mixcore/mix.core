b6425a10daa80523c858ecb7890f45cf41377cb9
Huy Nguyen <Smilefounder@users.noreply.github.com>
2018-11-03 21:16:00 +0700

    fix app init

--- src/Mix.Cms.Web/app/app-init/app.route.js
+++ src/Mix.Cms.Web/app/app-init/app.route.js
@@ -3,17 +3,17 @@ app.config(function ($routeProvider, $locationProvider, $sceProvider) {
 
     $routeProvider.when("/init/login", {
         controller: "loginController",
-        templateUrl: "/app-init/pages/login/login.html"
+        templateUrl: "/app/app-init/pages/login/login.html"
     });
 
     $routeProvider.when("/init", {
         controller: "Step1Controller",
-        templateUrl: "/app-init/pages/step1/index.html"
+        templateUrl: "/app/app-init/pages/step1/index.html"
     });
 
     $routeProvider.when("/init/step2", {
         controller: "Step2Controller",
-        templateUrl: "/app-init/pages/step2/index.html"
+        templateUrl: "/app/app-init/pages/step2/index.html"
     });
 
     $routeProvider.otherwise({ redirectTo: "/init" });
