b9f239933c3f353376f2bbcf8137500d1b3b74f7
Huy Nguyen <Smilefounder@users.noreply.github.com>
2018-11-04 15:22:02 +0700

    close #30

--- src/Mix.Cms.Web/App_Start/Startup.Auth.cs
+++ src/Mix.Cms.Web/App_Start/Startup.Auth.cs
@@ -1,5 +1,5 @@
-﻿// Licensed to the Mix I/O Foundation under one or more agreements.
-// The Mix I/O Foundation licenses this file to you under the GNU General Public License v3.0 license.
+﻿// Licensed to the mixcore Foundation under one or more agreements.
+// The mixcore Foundation licenses this file to you under the GNU General Public License v3.0 license.
 // See the LICENSE file in the project root for more information.
 
 using Microsoft.AspNetCore.Authentication.Cookies;
