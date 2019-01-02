193091f5c28218e00adcec743d9ee03e5f40e5da
Hoang Nguyen <nhathoang989@gmail.com>
2019-01-02 11:01:34 +0700

    fix init flow

--- src/Mix.Cms.Lib/Services/MixService.cs
+++ src/Mix.Cms.Lib/Services/MixService.cs
@@ -63,13 +63,13 @@ namespace Mix.Cms.Lib.Services
 
         private void LoadConfiggurations()
         {
-            var settings = FileRepository.Instance.GetFile(MixConstants.CONST_FILE_APPSETTING, ".json", string.Empty, true, "{}");
+            var settings = FileRepository.Instance.GetFile(MixConstants.CONST_FILE_APPSETTING, ".json", string.Empty, false, "{}");
 
             string content = string.IsNullOrWhiteSpace(settings.Content) ? "{}" : settings.Content;
             JObject jsonSettings = JObject.Parse(content);
             if (jsonSettings["GlobalSettings"] == null)
             {
-                settings = FileRepository.Instance.GetFile(MixConstants.CONST_DEFAULT_FILE_APPSETTING, ".json", string.Empty, true, "{}");
+                settings = FileRepository.Instance.GetFile(MixConstants.CONST_DEFAULT_FILE_APPSETTING, ".json", string.Empty, false, "{}");
                 jsonSettings = JObject.Parse(settings.Content);
 
             }
