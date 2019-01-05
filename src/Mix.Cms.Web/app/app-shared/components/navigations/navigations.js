53d0d8c206c17baf57ead6adb029901f59d56012
hoang <nhathoang989@gmail.com>
2019-01-05 23:51:57 +0700

    fix navigations.html

--- src/Mix.Cms.Web/app/app-shared/components/navigations/navigations.js
+++ src/Mix.Cms.Web/app/app-shared/components/navigations/navigations.js
@@ -4,11 +4,25 @@ modules.component('navs', {
     controller: ['$scope', '$location', function ($scope, $location) {
         var ctrl = this;
         ctrl.selected = null;
-        ctrl.updateOrders = function (index) {
-            ctrl.data.splice(index, 1);
+        ctrl.activedIndex = null;
+        ctrl.updateOrders = function (index) {       
+            ctrl.data.splice(index, 1);     
             for (var i = 0; i < ctrl.data.length; i++) {
                 ctrl.data[i].priority = i + 1;
             }
+        };
+        ctrl.dropCallback = function(index, item, external, type) {
+            ctrl.logListEvent('dropped at', index, external, type);
+            for (var i = 0; i < ctrl.data.length; i++) {
+                ctrl.data[i].priority = i + 1;
+            }
+            // Return false here to cancel drop. Return true if you insert the item yourself.
+            return item;
+        };
+        ctrl.logListEvent = function(action, index, external, type) {
+            var message = external ? 'External ' : '';
+            message += type + ' element was ' + action + ' position ' + index;
+            console.log(message);
         };
         ctrl.goToDetails = async function (nav) {
             $location.url(ctrl.detailsUrl + nav[ctrl.key]);
