4d5bfa0aeaab4eca8ba4d0ae0fbcd248176e553e
Hoang Nguyen <nhathoang989@gmail.com>
2019-01-11 16:31:37 +0700

    update portal list module articles

---
+++ src/Mix.Cms.Web/app/app-client/components/fb-like/fb-send.js
@@ -0,0 +1,22 @@
+﻿
+modules.component('fbLike', {
+    templateUrl: '/app/app-client/components/fb-like/fb-like.html',
+    controller: ['$location', function ($location) {
+        var ctrl = this;
+        ctrl.href = ctrl.href || window.top.location.href;        
+        ctrl.layout = ctrl.layout || 'standard';        
+        ctrl.size = ctrl.size || 'small';        
+        ctrl.showFaces = ctrl.showFaces || true;    
+        this.$onInit = function(){
+            setTimeout(() => {
+                FB.XFBML.parse();
+            }, 200);
+        }    
+    }],
+    bindings: {
+        href:'=',
+        layout:'=',
+        size:'=',
+        showFaces:'='
+    }
+});
