29142ca575cd1914df3ad716da8d5cec5ba86fb3
hoang <nhathoang989@gmail.com>
2019-01-24 08:17:25 +0700

    fix permission modal

--- src/Mix.Cms.Web/app/app-portal/pages/permission/components/plug-play/plug-play.js
+++ src/Mix.Cms.Web/app/app-portal/pages/permission/components/plug-play/plug-play.js
@@ -3,6 +3,8 @@ app.component('permissionPlugPlay', {
     templateUrl: '/app/app-portal/pages/permission/components/plug-play/plug-play.html',
     bindings: {
         page: '=',
+        prefixParent: '=',
+        prefixChild: '=',
         onDelete: '&',
         onUpdate: '&'
     }
