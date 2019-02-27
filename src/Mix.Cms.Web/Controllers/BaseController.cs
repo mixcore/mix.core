ab86187163b41aadc1887c26a17e2baaa0b32532
hoang <nhathoang989@gmail.com>
2019-02-27 21:27:43 +0700

    add retrict Ip

--- src/Mix.Cms.Web/Controllers/BaseController.cs
+++ src/Mix.Cms.Web/Controllers/BaseController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Globalization;
 using AutoMapper.Configuration;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -14,22 +15,26 @@ namespace Mix.Cms.Web.Controllers
     public class BaseController : Controller
     {
         protected string _domain;
+        protected bool _forbidden = false;
         protected IConfiguration _configuration;
         protected IHostingEnvironment _env;
         protected readonly IMemoryCache _memoryCache;
-        public BaseController(IHostingEnvironment env, IMemoryCache memoryCache)
+        protected readonly IHttpContextAccessor _accessor;
+        public BaseController(IHostingEnvironment env, IMemoryCache memoryCache, IHttpContextAccessor accessor)
         {
             _env = env;
             _memoryCache = memoryCache;
+            _accessor = accessor;
             // Set CultureInfo
             var cultureInfo = new CultureInfo(_culture);
             CultureInfo.DefaultThreadCurrentCulture = cultureInfo;
             CultureInfo.DefaultThreadCurrentUICulture = cultureInfo;
         }
 
-        public BaseController(IHostingEnvironment env, IConfiguration configuration)
+        public BaseController(IHostingEnvironment env, IConfiguration configuration, IHttpContextAccessor accessor)
         {
             _configuration = configuration;
+            _accessor = accessor;
             _env = env;
             // Set CultureInfo
             var cultureInfo = new CultureInfo(_culture);
@@ -48,6 +53,21 @@ namespace Mix.Cms.Web.Controllers
         {
             ViewBag.culture = _culture;
             _domain = string.Format("{0}://{1}", Request.Scheme, Request.Host);
+            if (MixService.GetConfig<bool>("IsRetrictIp"))
+            {
+                string allowedIps = MixService.GetConfig<string>("AllowedIps");
+                string exceptIps = MixService.GetConfig<string>("ExceptIps");
+                string remoteIp = _accessor.HttpContext?.Connection?.RemoteIpAddress?.ToString();
+                if (
+                    // allow localhost
+                    remoteIp != "::1" &&
+                    (allowedIps != "*" && !allowedIps.Contains(remoteIp))
+                    || (exceptIps.Contains(remoteIp))
+                    )
+                {
+                    _forbidden = true;
+                }
+            }
             base.OnActionExecuting(context);
         }
 
