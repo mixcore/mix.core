a653b35621497f65b50f47bce6a5f560be4b7c21
hoang <nhathoang989@gmail.com>
2019-03-05 00:17:18 +0700

    add position ReadListItemViewModel

---
+++ src/Mix.Cms.Lib/ViewModels/MixPagePositions/ReadListItemViewModel.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Storage;
+using Mix.Cms.Lib.Models.Cms;
+using Mix.Domain.Data.ViewModels;
+using Newtonsoft.Json;
+
+namespace Mix.Cms.Lib.ViewModels.MixPagePositions
+{
+    public class ReadListItemViewModel
+       : ViewModelBase<MixCmsContext, MixPagePosition, ReadListItemViewModel>
+    {
+        public ReadListItemViewModel(MixPagePosition model, MixCmsContext _context = null, IDbContextTransaction _transaction = null)
+            : base(model, _context, _transaction)
+        {
+        }
+
+        public ReadListItemViewModel() : base()
+        {
+        }
+
+        [JsonProperty("PositionId")]
+        public int PositionId { get; set; }
+
+        [JsonProperty("categoryId")]
+        public int CategoryId { get; set; }
+
+        [JsonProperty("isActived")]
+        public bool IsActived { get; set; }
+
+        [JsonProperty("image")]
+        public string Image { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+    }
+}
