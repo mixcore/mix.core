b62c37dcfc02757967e5a8270a6b6b17a3b49f80
hoang <nhathoang989@gmail.com>
2019-03-13 12:04:38 +0700

    fix Alias.cshtml

--- src/Mix.Cms.Web/app/app-portal/components/url-alias/url-alias.js
+++ src/Mix.Cms.Web/app/app-portal/components/url-alias/url-alias.js
@@ -1,31 +1,37 @@
 ﻿
 modules.component('urlAlias', {
     templateUrl: '/app/app-portal/components/url-alias/url-alias.html',
-    controller: ['$rootScope',  '$scope', 'UrlAliasService', 
+    controller: ['$rootScope', '$scope', 'UrlAliasService',
         function ($rootScope, $scope, service) {
-        var ctrl = this;        
-        ctrl.remove = function () {
-            $rootScope.showConfirm(ctrl, 'removeConfirmed', [ctrl.urlAlias.id], null, 'Remove', 'Are you sure');
-        };
-
-        ctrl.removeConfirmed = async function (id) {
-            $rootScope.isBusy = true;
-            var result = await service.delete(id);
-            if (result.isSucceed) {
-                if (ctrl.removeCallback) {
-                    ctrl.removeCallback({ index: ctrl.index });
+            var ctrl = this;
+            ctrl.remove = function () {
+                if (ctrl.urlAlias.id > 0) {
+                    $rootScope.showConfirm(ctrl, 'removeConfirmed', [ctrl.urlAlias.id], null, 'Remove', 'Are you sure');
+                } else {
+                    if (ctrl.removeCallback) {
+                        ctrl.removeCallback({ index: ctrl.index });
+                    }
                 }
-                $rootScope.isBusy = false;
-                $scope.$apply();
-            }
-            else {
-                $rootScope.showMessage('failed');
-                $rootScope.isBusy = false;
-                $scope.$apply();
-            }
-        };
+            };
 
-    }],
+            ctrl.removeConfirmed = async function (id) {
+                $rootScope.isBusy = true;
+                var result = await service.delete(id);
+                if (result.isSucceed) {
+                    if (ctrl.removeCallback) {
+                        ctrl.removeCallback({ index: ctrl.index });
+                    }
+                    $rootScope.isBusy = false;
+                    $scope.$apply();
+                }
+                else {
+                    $rootScope.showMessage('failed');
+                    $rootScope.isBusy = false;
+                    $scope.$apply();
+                }
+            };
+
+        }],
     bindings: {
         urlAlias: '=',
         index: '=',
