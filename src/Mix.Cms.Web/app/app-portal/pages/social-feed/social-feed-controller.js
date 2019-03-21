e0076d0f3aa919cd80673f6be672d57d050b7ea9
hoang <nhathoang989@gmail.com>
2019-03-21 20:43:28 +0700

    add func parse social feed  to article

--- src/Mix.Cms.Web/app/app-portal/pages/social-feed/social-feed-controller.js
+++ src/Mix.Cms.Web/app/app-portal/pages/social-feed/social-feed-controller.js
@@ -1,128 +1,172 @@
 ﻿'use strict';
-app.controller('SocialFeedController', 
-    ['$rootScope', '$scope', '$http', 
-    function ($rootScope, $scope, $http) {
-        $scope.types = [
-          'Facebook',
-          'Instagram'
-        ];
-        $scope.isInit = false;
-        $scope.data = [];
-        $scope.errors = [];
-        $scope.socialSettings = {
-            app_id: null,
-            page_id: null,
-            app_secret: null,
-            access_token:'',
-            page:[],
-            show_login: true,
-            errors:[]
-        };
-        $scope.init = function () {
+app.controller('SocialFeedController',
+    ['$rootScope', '$scope', '$http',
+        function ($rootScope, $scope, $http) {
+            $scope.types = [
+                'Facebook',
+                'Instagram'
+            ];
+            $scope.isInit = false;
+            $scope.data = [];
+            $scope.errors = [];
             $scope.socialSettings = {
-                app_id: $rootScope.settings.data.FacebookAppId,
-                page_id: $rootScope.settings.data.Facebook_Page_Id,
-                app_secret: $rootScope.settings.data.FacebookAppSecret,
-                access_token:'',
+                app_id: null,
+                page_id: null,
+                app_secret: null,
+                access_token: '',
+                page: [],
+                data: [],
+                articles:[],
                 show_login: true,
-                errors:[]
+                errors: []
             };
-        };
-        
-        // This function is called when someone finishes with the Login
-        // Button.  See the onlogin handler attached to it in the sample
-        // code below.
-        $scope.login = function () {
-            FB.login(function (response) {
-                // handle the response
-                $scope.statusChangeCallback(response);
+            $scope.init = function () {
+                $scope.socialSettings = {
+                    app_id: $rootScope.settings.data.FacebookAppId,
+                    page_id: $rootScope.settings.data.Facebook_Page_Id,
+                    app_secret: $rootScope.settings.data.FacebookAppSecret,
+                    access_token: $rootScope.settings.data.FacebookAccessToken,
+                    show_login: true,
+                    errors: []
+                };
+                if ($scope.socialSettings.access_token) {
+                    $scope.socialSettings.show_login = false;
+                    $scope.loadPages();
 
-            }, {
-                scope: 'email, manage_pages',
-                return_scopes: true
-            });
-        }
-        
-        $scope.statusChangeCallback = function (response) {
-            // The response object is returned with a status field that lets the
-            // app know the current login status of the person.
-            // Full docs on the response object can be found in the documentation
-            // for FB.getLoginStatus().
-            if (response.status === 'connected') {
-                // Logged into your app and Facebook.
-                $scope.exchangeToken(response);
-                $scope.loadPages();
-                //window.location = '/bo/feed?code=' + response.authResponse.accessToken;
-            } else {
-                alert('Login failed');
-                // The person is not logged into your app or we are unable to tell.                    
+                }
+            };
+
+            // This function is called when someone finishes with the Login
+            // Button.  See the onlogin handler attached to it in the sample
+            // code below.
+            $scope.login = function () {
+                FB.login(function (response) {
+                    // handle the response
+                    $scope.statusChangeCallback(response);
+
+                }, {
+                        scope: 'email, manage_pages',
+                        return_scopes: true
+                    });
             }
-        }
 
-        $scope.exchangeToken = function (response) {
-            var url = '/oauth/access_token?grant_type=fb_exchange_token&client_id='+ $scope.socialSettings.app_id +'&client_secret='+ $scope.socialSettings.app_secret +'&fb_exchange_token=' + response.authResponse.accessToken;
-            FB.api(url, function (response) {
-                if(response.access_token) {
-                    $scope.socialSettings.access_token = response.access_token;
-                }
-                else{
-                    $scope.show_login = true;
-                    $scope.socialSettings.errors = response;
-                    $scope.$apply();
-                }
-            });
-        }
-        $scope.loadFeeds = function (url) {
-            $scope.socialSettings.errors = '';
-            url = url || '/' + $scope.socialSettings.page_id + '/posts?access_token=' + $scope.socialSettings.access_token + '&fields=name,story,created_time,permalink_url,message,description,caption,attachments,shares.summary(true).limit(0),likes.summary(true).limit(0),comments.summary(true).limit(0)&limit=6';
-            FB.api(url, function (response) {
-                if (response.data) {
-                    $scope.socialSettings.data = response.data;
-                    $scope.socialSettings.nextUrl = response.paging.next;
-                    $scope.socialSettings.prevUrl = response.paging.previous;
-                    $scope.$apply();
-                }
-                else {
+            $scope.statusChangeCallback = function (response) {
+                // The response object is returned with a status field that lets the
+                // app know the current login status of the person.
+                // Full docs on the response object can be found in the documentation
+                // for FB.getLoginStatus().
+                if (response.status === 'connected') {
+                    // Logged into your app and Facebook.
+                    $scope.exchangeToken(response);
+                    $scope.loadPages();
+                    //window.location = '/bo/feed?code=' + response.authResponse.accessToken;
+                } else {
                     $scope.socialSettings.show_login = true;
-                    $scope.socialSettings.errors = response;
-                }
-                console.log(response);
-            });
-        }
-        $scope.loadPages = function () {
-            $scope.socialSettings.errors = '';
-            var url = '/me/accounts?access_token=' + $scope.socialSettings.access_token + '&fields=id,name';
-            FB.api(url, function (response) {
-                if (response.data) {
-                    $scope.socialSettings.pages = response.data;
                     $scope.$apply();
+                    // The person is not logged into your app or we are unable to tell.                    
                 }
-                else {
-                    $scope.socialSettings.show_login = true;
-                    $scope.socialSettings.errors = response;
-                }
-                console.log(response);
-            });
-        }
-        $scope.setAttr = function (e, attrName, attVal) {
-            $(e).attr(attrName, attVal);
-        };
-        $scope.addPage = function(){
-            var page={
-                page_name:'',
-                page_id:'',
-                access_token:'',
-                show_login: true,
-                is_edit:true
             }
-            $scope.pages.push(page);
-            $scope.socialSettings.pages.push(page);
-        }
 
-        $scope.removePage = function(index){
-            $scope.pages.splice(index,1);
-            $scope.socialSettings.pages.splice(index,1);
-            $scope.strSettings = JSON.stringify($scope.socialSettings);
-            $('.data').val(JSON.stringify($scope.socialSettings));
-        }
-    }]);
+            $scope.exchangeToken = function (response) {
+                var url = '/oauth/access_token?grant_type=fb_exchange_token&client_id=' + $scope.socialSettings.app_id + '&client_secret=' + $scope.socialSettings.app_secret + '&fb_exchange_token=' + response.authResponse.accessToken;
+                FB.api(url, function (response) {
+                    if (response.access_token) {
+                        $scope.socialSettings.access_token = response.access_token;
+                    }
+                    else {
+                        $scope.show_login = true;
+                        $scope.socialSettings.errors = response;
+                        $scope.$apply();
+                    }
+                });
+            }
+            $scope.loadFeeds = function (url) {
+                $scope.socialSettings.errors = '';
+                $scope.socialSettings.articles = [];
+                url = url || '/' + $scope.socialSettings.page_id + '/posts?access_token=' + $scope.socialSettings.access_token + '&fields=type,name,story,full_picture,created_time,permalink_url,message,description,caption,attachments{subattachments},shares.summary(true).limit(0),likes.summary(true).limit(0),comments.summary(true).limit(0)&limit=10';
+                $rootScope.isBusy = true;
+                FB.api(url, function (response) {
+                    if (response.data) {
+                        $scope.socialSettings.data = response.data;
+                        angular.forEach(response.data, function(e,i){
+                            var article = $scope.parsePost(e);
+                            $scope.socialSettings.articles.push(article);
+                        });
+                        if (response.paging) {
+                            $scope.socialSettings.nextUrl = response.paging.next;
+                            $scope.socialSettings.prevUrl = response.paging.previous;
+                        }
+                        $rootScope.isBusy = false;
+                        $scope.$apply();
+                    }
+                    else {
+                        $scope.socialSettings.show_login = true;
+                        $rootScope.isBusy = false;
+                        $scope.socialSettings.errors = response;
+                        $scope.$apply();
+                    }
+                    console.log(response);
+                });
+            }
+            $scope.loadPages = function () {
+                $scope.socialSettings.errors = '';
+                var url = '/me/accounts?access_token=' + $scope.socialSettings.access_token + '&fields=id,name';
+                FB.api(url, function (response) {
+                    if (response.data) {
+                        $scope.socialSettings.pages = response.data;
+                        $scope.$apply();
+                    }
+                    else {
+                        $scope.socialSettings.show_login = true;
+                        $scope.socialSettings.errors = response;
+                    }
+                });
+            }
+            $scope.setAttr = function (e, attrName, attVal) {
+                $(e).attr(attrName, attVal);
+            };
+            $scope.parsePost = function(post){
+                var article = {
+                    title: post.name || post.id,
+                    excerpt: post.message,
+                    content: post.description,
+                    image: post.full_picture,
+                    mediaNavs: []
+
+                };
+                if(post.attachments){
+                    var medias = $scope.parseMedias(post.attachments.data[0].subattachments.data);
+                    angular.forEach(medias, function(e,i){
+                        article.mediaNavs.push({
+                            media: e,
+                            specificulture: $rootScope.settings.lang,
+                            image: e.fullPath
+                        })
+                    })
+                }
+                return article;
+            }
+            $scope.parseMedias = function(data){
+                var result = [];
+                if (data) {
+                    angular.forEach(data, function (e, i) {
+                        if (e.media) {
+                            var index = e.media.image.src.lastIndexOf('/');
+                            var eIndex  = e.media.image.src.lastIndexOf('.')|| e.media.image.src.length;
+                            var qIndex  = e.media.image.src.lastIndexOf('?')|| e.media.image.src.length;
+                            var folder = e.media.image.src.substring(0,index);
+                            var filename =e.media.image.src.substring(index,eIndex);
+                            var extension = e.media.image.src.substring(eIndex,qIndex);
+                            var media = {
+                                fileFolder: folder,
+                                fileName: filename,
+                                extension: extension,
+                                fullPath: folder + '/' + filename + extension
+                            }
+                            result.push(media);
+                        }
+                    })
+                }
+                return result;
+            }
+        }]);
