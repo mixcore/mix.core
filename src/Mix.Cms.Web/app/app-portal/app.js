e5f032bc295bbd51928614c6436e25418984b7ef
Hoang Nguyen <nhathoang989@gmail.com>
2019-05-26 01:29:41 +0700

    clean src

--- src/Mix.Cms.Web/app/app-portal/app.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-var app = angular.module('MixPortal', ['ngRoute', 'components', 'ngFileUpload', 'LocalStorageModule',
-    'bw.paging', 'dndLists', 'ngTagsInput', 'ngSanitize']);
-var modules = angular.module('components', []);
