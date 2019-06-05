3bcfcc0ab2ab47eaba4fc211105bd81a99cf4e72
Hoang Nguyen <nhathoang989@gmail.com>
2019-06-05 09:52:33 +0700

    update portal preview image style

---
+++ src/portal-app/app/app-portal/pages/attribute-set/_fieldServices.js
@@ -0,0 +1,8 @@
+﻿'use strict';
+app.factory('AttributeFieldService', ['BaseService',
+    function (baseService) {
+        baseService.init('attribute-field');
+        var serviceFactory = baseService;
+        // Define more service methods here
+    return serviceFactory;
+}]);
---
+++ src/portal-app/app/app-portal/pages/attribute-set/_services.js
@@ -0,0 +1,8 @@
+﻿'use strict';
+app.factory('AttributeSetService', ['BaseService',
+    function (baseService) {
+        baseService.init('attribute-set');
+        var serviceFactory = baseService;
+        // Define more service methods here
+    return serviceFactory;
+}]);
