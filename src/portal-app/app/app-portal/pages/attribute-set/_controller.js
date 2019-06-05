6dc737ca48db18652f4332c81f8219bbd8108f02
Hoang Nguyen <nhathoang989@gmail.com>
2019-06-05 09:52:33 +0700

    update portal preview image style

---
+++ src/portal-app/app/app-portal/pages/attribute-set/_controller.js
@@ -0,0 +1,20 @@
+﻿'use strict';
+app.controller('AttributeSetController', [
+    '$scope', '$rootScope', '$location',
+    'ngAppSettings', '$routeParams', 'AttributeFieldService', 'AttributeSetService',      
+    function ($scope, $rootScope, $location, 
+        ngAppSettings, $routeParams, attributeFieldService, service) {
+        BaseCtrl.call(this, $scope, $rootScope, $routeParams, ngAppSettings, service);
+        $scope.defaultAttr = null;
+        $scope.getSingleSuccessCallback = async function () {
+            var getDefaultAttr = await attributeFieldService.getSingle([null, 'portal']);
+            if (getDefaultAttr.isSucceed) {
+                $scope.defaultAttr = getDefaultAttr.data;
+            }
+            $scope.$apply();
+        }
+        $scope.saveCallback = function () {
+            $location.url($scope.referrerUrl);
+        };
+    }
+]);
