668b7d87b0c83016da52436e5538ff5ab4d3f894
Hoang Nguyen <nhathoang989@gmail.com>
2019-06-07 08:48:06 +0700

    add monaco command, event

--- src/portal-app/app/app-portal/components/monaco-editor/script.js
+++ src/portal-app/app/app-portal/components/monaco-editor/script.js
@@ -60,43 +60,15 @@
                         }
                         ctrl.editor = monaco.editor.create(e, model);
                         
-                        // editor.setTheme("ace/theme/chrome");
-                        // //editor.setReadOnly(true);
-                        // if (ctrl.content) {
-                        //     editor.setValue(ctrl.content);
-                        // }
-                        // editor.$blockScrolling = Infinity;
-                        // editor.session.setUseWrapMode(true);
-                        // editor.setOptions({
-                        //     enableBasicAutocompletion: true,
-                        //     enableSnippets: true,
-                        //     enableLiveAutocompletion: false,
-                        //     maxLines: 50,
-                        //     fontSize: 11
-                        // });
-                        // editor.getSession().on('change', function (e) {
-                        //     // e.type, etc
-                        //     ctrl.content = editor.getValue();
-                        // });
-                        // editor.getSession().on('paste', function (e) {
-                        //     // e.type, etc
-                        //     ctrl.content = editor.getValue();
-                        // });
-                        // editor.commands.addCommand({
-                        //     name: 'saveFile',
-                        //     bindKey: {
-                        //     win: 'Ctrl-S',
-                        //     mac: 'Command-S',
-                        //     sender: 'editor|cli'
-                        //     },
-                        //     exec: function(env, args, request) {
-                                
-                        //        var btn = document.getElementById('btnToSubmit');
-                        //        btn.click();
-                        //     }
-                        //     });
+                        ctrl.editor.model.onDidChangeContent(()=>{
+                            ctrl.content = ctrl.editor.model.getValue();
+                        });
+                        ctrl.editor.addCommand(monaco.KeyMod.CtrlCmd | monaco.KeyCode.KEY_S, function() {
+                            var btn = document.getElementById('btnToSubmit');
+                            btn.click();
+                        });
                         setTimeout(() => {
-                            var h = ctrl.editor.getModel().getLineCount() * 8;
+                            var h = ctrl.editor.getModel().getLineCount() * 18;
                             $(e).height(h);
                             ctrl.editor.layout();    
                         }, 200);
