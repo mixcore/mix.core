8f66453ef111f5d47959a4ec725cb5cf7d5e65db
Huy Nguyen <i.love.to.smile.around@gmail.com>
2019-07-13 16:29:13 +0700

    upd input palce holder color

---
+++ src/portal-app/lib/portal/css-var.ps1
@@ -0,0 +1,28 @@
+((Get-Content -path mix.fe.portal-mod.css -Raw) -replace '#000','hsla(var(--primary-color-hue,211), var(--primary-color-saturation,100%), 0%, 1)') | Set-Content -Path mix.fe.portal-mod.css
+((Get-Content -path mix.fe.portal-mod.css -Raw) -replace '#002d5d','hsla(var(--primary-color-hue,211), var(--primary-color-saturation,100%), 5%, 1)') | Set-Content -Path mix.fe.portal-mod.css
+((Get-Content -path mix.fe.portal-mod.css -Raw) -replace '#003773','hsla(var(--primary-color-hue,211), var(--primary-color-saturation,100%), 10%, 1)') | Set-Content -Path mix.fe.portal-mod.css
+((Get-Content -path mix.fe.portal-mod.css -Raw) -replace '#003d80','hsla(var(--primary-color-hue,211), var(--primary-color-saturation,100%), 15%, 1)') | Set-Content -Path mix.fe.portal-mod.css
+((Get-Content -path mix.fe.portal-mod.css -Raw) -replace '#004085','hsla(var(--primary-color-hue,211), var(--primary-color-saturation,100%), 20%, 1)') | Set-Content -Path mix.fe.portal-mod.css
+((Get-Content -path mix.fe.portal-mod.css -Raw) -replace '#00448d','hsla(var(--primary-color-hue,211), var(--primary-color-saturation,100%), 25%, 1)') | Set-Content -Path mix.fe.portal-mod.css
+((Get-Content -path mix.fe.portal-mod.css -Raw) -replace '#0056b3','hsla(var(--primary-color-hue,211), var(--primary-color-saturation,100%), 30%, 1)') | Set-Content -Path mix.fe.portal-mod.css
+((Get-Content -path mix.fe.portal-mod.css -Raw) -replace '#005cbf','hsla(var(--primary-color-hue,211), var(--primary-color-saturation,100%), 35%, 1)') | Set-Content -Path mix.fe.portal-mod.css
+((Get-Content -path mix.fe.portal-mod.css -Raw) -replace '#0062cc','hsla(var(--primary-color-hue,211), var(--primary-color-saturation,100%), 40%, 1)') | Set-Content -Path mix.fe.portal-mod.css
+((Get-Content -path mix.fe.portal-mod.css -Raw) -replace '#0069d9','hsla(var(--primary-color-hue,211), var(--primary-color-saturation,100%), 45%, 1)') | Set-Content -Path mix.fe.portal-mod.css
+((Get-Content -path mix.fe.portal-mod.css -Raw) -replace '#007bff','hsla(var(--primary-color-hue,211), var(--primary-color-saturation,100%),var(--primary-color-brightness,50%), 1)') | Set-Content -Path mix.fe.portal-mod.css
+((Get-Content -path mix.fe.portal-mod.css -Raw) -replace '#409cff','hsla(var(--primary-color-hue,211), var(--primary-color-saturation,100%), 54%, 1)') | Set-Content -Path mix.fe.portal-mod.css
+((Get-Content -path mix.fe.portal-mod.css -Raw) -replace '#4da2ff','hsla(var(--primary-color-hue,211), var(--primary-color-saturation,100%), 58%, 1)') | Set-Content -Path mix.fe.portal-mod.css
+((Get-Content -path mix.fe.portal-mod.css -Raw) -replace '#5aa9ff','hsla(var(--primary-color-hue,211), var(--primary-color-saturation,100%), 62%, 1)') | Set-Content -Path mix.fe.portal-mod.css
+((Get-Content -path mix.fe.portal-mod.css -Raw) -replace '#7abaff','hsla(var(--primary-color-hue,211), var(--primary-color-saturation,100%), 66%, 1)') | Set-Content -Path mix.fe.portal-mod.css
+((Get-Content -path mix.fe.portal-mod.css -Raw) -replace '#80bdff','hsla(var(--primary-color-hue,211), var(--primary-color-saturation,100%), 70%, 1)') | Set-Content -Path mix.fe.portal-mod.css
+((Get-Content -path mix.fe.portal-mod.css -Raw) -replace '#9fcdff','hsla(var(--primary-color-hue,211), var(--primary-color-saturation,100%), 74%, 1)') | Set-Content -Path mix.fe.portal-mod.css
+((Get-Content -path mix.fe.portal-mod.css -Raw) -replace '#b8daff','hsla(var(--primary-color-hue,211), var(--primary-color-saturation,100%), 78%, 1)') | Set-Content -Path mix.fe.portal-mod.css
+((Get-Content -path mix.fe.portal-mod.css -Raw) -replace '#bdddff','hsla(var(--primary-color-hue,211), var(--primary-color-saturation,100%), 82%, 1)') | Set-Content -Path mix.fe.portal-mod.css
+((Get-Content -path mix.fe.portal-mod.css -Raw) -replace '#c2e0ff','hsla(var(--primary-color-hue,211), var(--primary-color-saturation,100%), 86%, 1)') | Set-Content -Path mix.fe.portal-mod.css
+((Get-Content -path mix.fe.portal-mod.css -Raw) -replace '#cce5ff','hsla(var(--primary-color-hue,211), var(--primary-color-saturation,100%), 90%, 1)') | Set-Content -Path mix.fe.portal-mod.css
+((Get-Content -path mix.fe.portal-mod.css -Raw) -replace '#dbedff','hsla(var(--primary-color-hue,211), var(--primary-color-saturation,100%), 94%, 1)') | Set-Content -Path mix.fe.portal-mod.css
+((Get-Content -path mix.fe.portal-mod.css -Raw) -replace '#e6f2ff','hsla(var(--primary-color-hue,211), var(--primary-color-saturation,100%), 98%, 1)') | Set-Content -Path mix.fe.portal-mod.css
+((Get-Content -path mix.fe.portal-mod.css -Raw) -replace '#fff','hsla(var(--primary-color-hue,211), var(--primary-color-saturation,100%), 100%, 1)') | Set-Content -Path mix.fe.portal-mod.css
+
+
+((Get-Content -path mix.fe.portal-mod.css -Raw) -replace 'rgba\(0, 123, 255','hsla(var(--primary-color-hue,211), 100%, 50%') | Set-Content -Path mix.fe.portal-mod.css
+((Get-Content -path mix.fe.portal-mod.css -Raw) -replace 'rgba\(38, 143, 255','hsla(var(--primary-color-hue,211), 100%, 50%') | Set-Content -Path mix.fe.portal-mod.css
