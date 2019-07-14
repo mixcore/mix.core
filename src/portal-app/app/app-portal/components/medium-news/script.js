01aa7df1c9a1825e50890aff42a575bead098483
nhathoang <nhathoang989@gmail.com>
2019-07-14 16:18:54 +0700

    fix medium news

--- src/portal-app/app/app-portal/components/medium-news/script.js
+++ src/portal-app/app/app-portal/components/medium-news/script.js
@@ -1,16 +1,28 @@
 ﻿modules.component('mediumNews', {
   templateUrl: '/app/app-portal/components/medium-news/view.html',
   controller: [
-    '$rootScope', 'CommonService', '$http',
-    function ($rootScope, commonService,  $http) {
+    '$rootScope', '$http',
+    function ($rootScope, $http) {
       var ctrl = this;
+      ctrl.items = [];
       ctrl.init = function () {
-        var items = JSON.parse(_getMediumApiResult());
-      }
+        var req= {
+          method: 'GET',
+          url: 'https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/mixcore'
+        };
+        ctrl.getMediumApiResult(req);
+      };
 
-      var _getMediumApiResult = async function (req) {
-        return $http('https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/mixcore').then(function (resp) {
-          return resp.items;
+      ctrl.getMediumApiResult = async function (req) {
+        return $http(req).then(function (resp) {
+          if(resp.status == 'ok')
+          {
+              ctrl.items = resp.data.items;
+          }
+          else{
+            console.log(resp);
+ 
+          }
         },
           function (error) {
             return { isSucceed: false, errors: [error.statusText || error.status] };
@@ -19,8 +31,5 @@
     }
   ],
   bindings: {
-    totalPriceStatus: '=',
-    currencyCode: '=',
-    totalPrice: '='
   }
 });
