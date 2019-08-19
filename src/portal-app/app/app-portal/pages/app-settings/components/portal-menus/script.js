c35001bf5f040f7bd715260190be7e2348f694af
nhathoang <nhathoang989@gmail.com>
2019-08-19 21:08:43 +0700

    fix encrypt

---
+++ src/portal-app/app/app-portal/pages/app-settings/components/portal-menus/script.js
@@ -0,0 +1,14 @@
+﻿
+modules.component('portalMenus', {
+    templateUrl: '/app/app-portal/pages/app-settings/components/portal-menus/view.html',
+    controller: ['$rootScope', '$scope', '$location', function ($rootScope, $scope, $location) {
+        var ctrl = this;
+        ctrl.data = [];
+        ctrl.translate = $rootScope.translate;
+        ctrl.$onInit = function(){
+
+        };
+    }],
+    bindings: {
+    }
+});
