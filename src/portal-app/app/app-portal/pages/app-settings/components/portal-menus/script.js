71ab9064f4d27a994c85e4c16774ba16fc70a8f7
nhathoang <nhathoang989@gmail.com>
2019-08-19 21:16:58 +0700

    add portal menus tree view

--- src/portal-app/app/app-portal/pages/app-settings/components/portal-menus/script.js
+++ src/portal-app/app/app-portal/pages/app-settings/components/portal-menus/script.js
@@ -1,14 +1,18 @@
 ﻿
 modules.component('portalMenus', {
     templateUrl: '/app/app-portal/pages/app-settings/components/portal-menus/view.html',
-    controller: ['$rootScope', '$scope', '$location', function ($rootScope, $scope, $location) {
-        var ctrl = this;
-        ctrl.data = [];
-        ctrl.translate = $rootScope.translate;
-        ctrl.$onInit = function(){
-
-        };
-    }],
     bindings: {
-    }
+        'data': '=',
+        'allowedTypes': '='
+    },
+    controller: ['$rootScope', '$scope', '$location', 'CommonService', 'ngAppSettings', 
+    function ($rootScope, $scope, $location, commonService, ngAppSettings) {
+        var ctrl = this;
+        // ctrl.icons = [];
+        ctrl.translate = $rootScope.translate;  
+        ctrl.init = function(){
+            ctrl.icons = ngAppSettings.icons;      
+        };
+       
+    }]
 });
