e435b34178bc3744833e8c48026741c908eada91
nhathoang <nhathoang989@gmail.com>
2019-08-22 14:51:55 +0700

    init routes

--- src/portal-app/app/app-portal/app.route.js
+++ src/portal-app/app/app-portal/app.route.js
@@ -1,36 +1,5 @@
 app.config(function ($routeProvider, $locationProvider, $sceProvider, CommonServiceProvider) {
     $locationProvider.html5Mode(true);
-    var commonService  =CommonServiceProvider.$get();
-    commonService.loadJArrayData('portal-menus.json').then(resp=>{
-        angular.forEach(resp.data, function(item, key) {
-            if(item.type=='item'){
-                $routeProvider.when(item.path, {
-                    controller: item.controller,
-                    templateUrl: item.templatePath
-                });
-                angular.forEach(item.subMenus.data, function(sub, key) {
-                    $routeProvider.when(sub.path, {
-                        controller: sub.controller,
-                        templateUrl: sub.templatePath
-                    });
-                });
-            }
-            else{
-                angular.forEach(item.data, function(sub, key) {
-                    $routeProvider.when(sub.path, {
-                        controller: sub.controller,
-                        templateUrl: sub.templatePath
-                    });
-                    angular.forEach(sub.subMenus.data, function(sub1, key) {
-                        $routeProvider.when(sub1.path, {
-                            controller: sub1.controller,
-                            templateUrl: sub1.templatePath
-                        });
-                    });
-                });
-            }
-        });
-    });
-    
+    $routeProviderReference = $routeProvider;
     $routeProvider.otherwise({ redirectTo: "/portal" });
 });
