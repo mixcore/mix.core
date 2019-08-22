40d7b928d34146590168c1ef559083041f8f957a
nhathoang <nhathoang989@gmail.com>
2019-08-22 14:51:55 +0700

    init routes

--- src/portal-app/app/app-portal/components/main-side-bar/main-side-bar.js
+++ src/portal-app/app/app-portal/components/main-side-bar/main-side-bar.js
@@ -4,13 +4,9 @@
         function ($rootScope, $scope, ngAppSettings, translatorService, commonService) {
         var ctrl = this;
         ctrl.items = [];
-        ctrl.init = async function () {
-            commonService.loadJArrayData('portal-menus.json').then(resp=>{
-                var root = resp.data[0];
-                ctrl.items = root.data;
-                $rootScope.isBusy = false;
-                $scope.$apply();
-            });
+        ctrl.init = function () {
+            var root = ngAppSettings.routes[0];
+            ctrl.items = root.data;
         };
     }],
     bindings: {
