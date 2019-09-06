4bb87c74ffbe83d1f8647745d68b53c44a40d9b7
Smilefounder <i.love.to.smile.around@gmail.com>
2019-09-06 20:31:36 +0700

    update License

--- src/Mix.Cms.Api/Controllers/v1/OData/AttributeSetDatas/ApiODataAttributeSetDataPortalController.cs
+++ src/Mix.Cms.Api/Controllers/v1/OData/AttributeSetDatas/ApiODataAttributeSetDataPortalController.cs
@@ -1,5 +1,5 @@
-﻿// Licensed to the Mix I/O Foundation under one or more agreements.
-// The Mix I/O Foundation licenses this file to you under the MIT license.
+﻿// Licensed to the Mixcore Foundation under one or more agreements.
+// The Mixcore Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
 using Microsoft.AspNetCore.Authorization;
