c8aa846ecb7e0b7ff8d8c6374d4562943be7f86b
Smilefounder <i.love.to.smile.around@gmail.com>
2019-09-29 13:17:01 +0700

    Revert "Merge branch 'develop' into feature/styling-portal"
    This reverts commit 95e0bd901271b78db3fcaf09a0dcb675a7edadfd.

--- src/Mix.Cms.Lib/Hubs/ServiceHub.cs
+++ src/Mix.Cms.Lib/Hubs/ServiceHub.cs
@@ -8,54 +8,29 @@ namespace Mix.Cms.Hub
 {
     public class ServiceHub : BaseSignalRHub
     {
-        // TODO Handle Join/Leave group
-        public void JoinGroup(string groupName, string username)
-        {           
-            AddUserToGroup(groupName, username);           
-        }
-
-        public Task LeaveGroup(string groupName)
-        {
-            string username = "test";
-            string msg = $"new user {username}";
-            string groupMsg = $"new user {username}";
-            return Task.WhenAll(
-               new Task[]{
-                    SendMessageToCaller(msg),
-                    SendMessageToGroups(groupMsg)
-               }
-           );
-        }
-
-        private void RemoveUserFromGroup(string groupName)
-        {
-            throw new NotImplementedException();
-        }
-
-        private Task AddUserToGroup(string groupName, string username)
-        {
-            string msg = $"new user {username}";
-            string groupMsg = $"new user {username}";
-            return Task.WhenAll(
-               new Task[]{
-                    SendMessageToCaller(msg),
-                    SendMessageToGroups(groupMsg)
-               }
-           );
-        }
-        public Task SendMessageToConnectionId(string connectionId, string message)
-        {
-            return Clients.Client(connectionId).SendAsync("ReceiveMessage", message);
-        }
-
-        public async Task SendMessageToAll(string user, string message)
+        public async Task SendMessage(string user, string message)
         {
             await Clients.All.SendAsync("ReceiveMessage", user, message);
         }
+
         public Task SendMessageToCaller(string message)
         {
             return Clients.Caller.SendAsync("ReceiveMessage", message);
         }
+
+        // TODO Handle Join/Leave group
+        public Task JoinGroup(string groupName, string username)
+        {
+            string msg = $"new user {username}";
+            AddUserToGroup(groupName, username);
+            return Clients.Group(groupName).SendAsync("ReceiveMessage", msg );
+        }
+
+        private void AddUserToGroup(string groupName, string username)
+        {
+            throw new NotImplementedException();
+        }
+
         public Task SendMessageToGroups(string message)
         {
             List<string> groups = new List<string>() { "SignalR Users" };
