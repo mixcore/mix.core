ca663a6d1abda05b0aefcca7cc48ab22976ec9ba
nhathoang <nhathoang989@gmail.com>
2019-11-21 12:54:54 +0700

    clean portal module

---
+++ src/portal-app/app/app-portal/pages/module/components/module-type/script.js
@@ -0,0 +1,11 @@
+﻿
+app.component('moduleType', {
+    templateUrl: '/app/app-portal/pages/module/components/module-type/view.html',
+    controller: ['$rootScope', function ($rootScope) {
+        var ctrl = this;
+        ctrl.settings = $rootScope.globalSettings;
+    }],
+    bindings: {
+        model: '=',
+    }
+});
