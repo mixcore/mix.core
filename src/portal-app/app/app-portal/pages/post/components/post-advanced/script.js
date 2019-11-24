10677bdc546be2308e436c79b2a3f6b8b07bf9fb
nhathoang <nhathoang989@gmail.com>
2019-11-24 10:34:01 +0700

    clean portal

---
+++ src/portal-app/app/app-portal/pages/post/components/post-advanced/script.js
@@ -0,0 +1,14 @@
+﻿
+app.component('postAdvanced', {
+    templateUrl: '/app/app-portal/pages/post/components/post-advanced/view.html',
+    bindings: {
+        model: '='
+    },
+    controller: ['$rootScope', '$scope',
+        function ($rootScope, $scope, attributeSetService) {
+            var ctrl = this;
+            ctrl.translate = $rootScope.translate;    
+                  
+        }
+    ]    
+});
