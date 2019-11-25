1e107d83a00b26cc86c159da9cbe781daec9e428
Smilefounder <i.love.to.smile.around@gmail.com>
2019-11-25 09:54:41 +0700

    upd

---
+++ src/portal-app/app/app-portal/pages/store/store-controller.js
@@ -0,0 +1,46 @@
+﻿'use strict';
+app.controller('StoreController',
+    ['$scope', '$rootScope', 'ngAppSettings', '$routeParams', '$location', 'StoreService', 'CommonService',
+        function ($scope, $rootScope, ngAppSettings, $routeParams, $location, service, commonService) {
+            BaseCtrl.call(this, $scope, $rootScope, $routeParams, ngAppSettings, service);
+            $scope.cates = ngAppSettings.enums.configuration_types;
+            $scope.settings = $rootScope.globalSettings;
+            $scope.saveSuccessCallback = function () {
+                commonService.initAllSettings().then(function () {
+                    $location.url($scope.referrerUrl);
+                });
+            }
+            $scope.removeCallback = function () {
+                commonService.initAllSettings().then(function () {
+                    $location.url($scope.referrerUrl);
+                });
+            }
+
+            $scope.getList = function(){
+                $scope.data.items = [];
+            };
+
+            // $scope.items = [];
+            // $scope.init = function () {
+            //     var req = {
+            //         method: 'GET',
+            //         url: 'https://api.github.com/repos/mixcore/mix.core/contributors'
+            //     };
+            //     $scope.getGithubApiResult(req);
+            // };
+
+            // $scope.getGithubApiResult = async function (req) {
+            //     return $http(req).then(function (resp) {
+            //         if (resp.status == '200') {
+            //             $scope.items = resp.data;
+            //         }
+            //         else {
+            //             console.log(resp);
+
+            //         }
+            //     },
+            //         function (error) {
+            //             return { isSucceed: false, errors: [error.statusText || error.status] };
+            //         });
+            // };
+        }]);
