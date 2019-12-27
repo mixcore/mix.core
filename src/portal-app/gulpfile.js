b6bdf5544b0ebce98971e8a673b06bdcd92bf746
nhathoang <nhathoang989@gmail.com>
2019-12-27 19:45:52 +0700

    Remove onsen

--- src/portal-app/gulpfile.js
+++ src/portal-app/gulpfile.js
@@ -110,12 +110,11 @@ paths.framework = {
         paths.scriptLib + "angularjs/angular-animate.min.js",
         paths.scriptLib + "angularjs/angular-sanitize.min.js",
         paths.scriptLib + "angularjs/ui-bootstrap-tpls-2.5.0.min.js",
-        paths.scriptLib + "onsen/onsenui.min.js",
-        paths.scriptLib + "onsen/angular-onsenui.js",
         paths.scriptLib + "jquery/jquery.min.js",
         paths.scriptLib + "jquery/jquery-ui.min.js",
         paths.scriptLib + "bootstrap/js/popper.min.js",
         paths.scriptLib + "bootstrap/js/bootstrap.min.js",
+        paths.scriptLib + "lazysizes-5.2.0/lazysizes.min.js",
     ],
     dest: paths.webroot + "js/framework.min.js"
 };
