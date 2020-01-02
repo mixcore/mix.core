04e7ddd1dfc8db4c14bf5f20aec7a1bc15ba8ee7
nhathoang <nhathoang989@gmail.com>
2020-01-02 21:49:01 +0700

    update export theme

--- src/portal-app/app/app-portal/pages/theme/components/theme-export-modules/script.js
+++ src/portal-app/app/app-portal/pages/theme/components/theme-export-modules/script.js
@@ -3,7 +3,7 @@ app.component('themeExportModules', {
     templateUrl: '/app/app-portal/pages/theme/components/theme-export-modules/view.html',
     controller: ['$rootScope', '$scope', 'ngAppSettings', function ($rootScope, $scope) {
         var ctrl = this;
-        ctrl.updateModuleExport = function(module){
+        ctrl.updateModuleExport = function(){
             ctrl.selectedExport.modules = angular.copy($rootScope.filterArray(ctrl.exportData.modules, ['isActived'], [true])); 
             angular.forEach(ctrl.selectedExport.modules,function(e){
                 e.data.items = angular.copy($rootScope.filterArray(e.data.items, ['isActived'], [true]));
@@ -11,6 +11,13 @@ app.component('themeExportModules', {
             
             
         };    
+        ctrl.selectAll = function (isSelectAll, arr) {
+            ctrl.selectedList.data = [];
+            angular.forEach(arr, function (e) {
+                e.isActived = isSelectAll;
+                ctrl.updateModuleExport();
+            });
+        };        
     }],
     bindings: {
         exportData: '=',
--- src/portal-app/app/app-portal/pages/theme/components/theme-export-pages/script.js
+++ src/portal-app/app/app-portal/pages/theme/components/theme-export-pages/script.js
@@ -3,19 +3,33 @@ app.component('themeExportPages', {
     templateUrl: '/app/app-portal/pages/theme/components/theme-export-pages/view.html',
     controller: ['$rootScope', '$scope', 'ngAppSettings', function ($rootScope, $scope) {
         var ctrl = this;
-        ctrl.updatePageExport = function(page){
-            ctrl.selectedExport.pages = angular.copy($rootScope.filterArray(ctrl.exportData.pages, ['isActived'], [true])); 
-            angular.forEach(ctrl.selectedExport.pages,function(e){
+        ctrl.updatePageExport = function () {
+            // Filter actived page
+            ctrl.selectedExport.pages = angular.copy($rootScope.filterArray(ctrl.exportData.pages, ['isActived'], [true]));
+
+            // Loop actived page 
+            angular.forEach(ctrl.selectedExport.pages, function (e) {
+                // filter list actived modules
                 e.moduleNavs = angular.copy($rootScope.filterArray(e.moduleNavs, ['isActived'], [true]));
-                angular.forEach(e.moduleNavs,function(n){
+
+                // Loop actived modules
+                angular.forEach(e.moduleNavs, function (n) {
+                    // filter list actived data
                     n.module.data.items = angular.copy($rootScope.filterArray(n.module.data.items, ['isActived'], [true]));
                     $rootScope.removeObjectByKey(ctrl.exportData.modules, 'id', n.moduleId);
                     $rootScope.removeObjectByKey(ctrl.selectedExport.modules, 'id', n.moduleId);
                 });
             });
-            
-            
-        };    
+
+
+        };
+        ctrl.selectAll = function (isSelectAll, arr) {
+            ctrl.selectedList.data = [];
+            angular.forEach(arr, function (e) {
+                e.isActived = isSelectAll;
+                ctrl.updatePageExport();
+            });
+        };        
     }],
     bindings: {
         exportData: '=',
