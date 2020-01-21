ca46c9a9d3ce28d4ab0f4ed46c914270c42adfc2
nhathoang <nhathoang989@gmail.com>
2020-01-21 11:06:07 +0700

    fix bugs

--- src/Mix.Cms.Lib/MixCmsHelper.cs
+++ src/Mix.Cms.Lib/MixCmsHelper.cs
@@ -39,21 +39,8 @@ namespace Mix.Cms.Lib
             {
                 switch (cate.Type)
                 {
-                    case MixPageType.Blank:
-                        foreach (var child in cate.Childs)
-                        {
-                            child.Page.DetailsUrl = Url.RouteUrl("Alias", new { culture, seoName = child.Page.SeoName });
-                        }
-                        break;
-
-                    case MixPageType.StaticUrl:
-                        cate.DetailsUrl = cate.StaticUrl;
-                        break;
-
                     case MixPageType.Home:
                     case MixPageType.ListPost:
-                    case MixPageType.Post:
-                    case MixPageType.Modules:
                     default:
                         cate.DetailsUrl = Url.RouteUrl("Alias", new { culture, seoName = cate.SeoName });
                         break;
@@ -81,21 +68,8 @@ namespace Mix.Cms.Lib
             {
                 switch (cate.Type)
                 {
-                    case MixPageType.Blank:
-                        foreach (var child in cate.Childs)
-                        {
-                            child.Page.DetailsUrl = Url.RouteUrl("Alias", new { culture, seoName = child.Page.SeoName });
-                        }
-                        break;
-
-                    case MixPageType.StaticUrl:
-                        cate.DetailsUrl = cate.StaticUrl;
-                        break;
-
                     case MixPageType.Home:
                     case MixPageType.ListPost:
-                    case MixPageType.Post:
-                    case MixPageType.Modules:
                     default:
                         cate.DetailsUrl = Url.RouteUrl("Alias", new { culture, seoName = cate.SeoName });
                         break;
