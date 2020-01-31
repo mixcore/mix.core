4a4e0c413345b2bf0d9ab0ed53448bcec338727b
nhathoang <nhathoang989@gmail.com>
2020-01-31 19:03:12 +0700

    upd

---
+++ src/Mix.Cms.Lib/ViewModels/MixRelatedData/ReadViewModel.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Storage;
+using Mix.Cms.Lib.Models.Cms;
+using Mix.Domain.Data.ViewModels;
+using Newtonsoft.Json;
+using System;
+
+namespace Mix.Cms.Lib.ViewModels.MixRelatedDatas
+{
+    public class ReadViewModel
+        : ViewModelBase<MixCmsContext, MixRelatedData, ReadViewModel>
+    {
+        #region Properties
+
+        #region Models
+        [JsonProperty("id")]
+        public string Id { get; set; }
+        [JsonProperty("parentId")]
+        public string ParentId { get; set; }
+        [JsonProperty("parentType")]
+        public int ParentType { get; set; }
+        [JsonProperty("attributeSetId")]
+        public int AttributeSetId { get; set; }
+        [JsonProperty("createdDateTime")]
+        public DateTime CreatedDateTime { get; set; }
+        [JsonProperty("status")]
+        public int Status { get; set; }
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        #endregion Models
+
+        #region Views
+
+        //[JsonProperty("isActived")]
+        //public bool IsActived { get; set; }
+
+        #endregion Views
+
+        #endregion Properties
+
+        #region Contructors
+
+        public ReadViewModel() : base()
+        {
+        }
+
+        public ReadViewModel(MixRelatedData model, MixCmsContext _context = null, IDbContextTransaction _transaction = null) : base(model, _context, _transaction)
+        {
+        }
+
+        #endregion Contructors
+
+        #region Overrides
+
+        public override void ExpandView(MixCmsContext _context = null, IDbContextTransaction _transaction = null)
+        {
+            //var getPost = MixAttributeSetDatas.ReadViewModel.Repository.GetSingleModel(
+            //    m => m.Id == Id && m.Specificulture == Specificulture
+            //    , _context: _context, _transaction: _transaction);
+            //if (getPost.IsSucceed)
+            //{
+            //    this.RelatedAttributeData = getPost.Data;
+            //}
+        }
+
+
+        #endregion Overrides
+    }
+}
