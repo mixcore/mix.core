c94cb2e4c2072080cc79d91d8421783f2153836e
nhathoang <nhathoang989@gmail.com>
2020-02-06 09:52:50 +0700

    fix reference

--- src/Mix.Cms.Web/Areas/Identity/IdentityHostingStartup.cs
+++ src/Mix.Cms.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
