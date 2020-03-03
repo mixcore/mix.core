70cd3f251ddd91fbd4368caad4d5b680197f5c95
nhathoang <nhathoang989@gmail.com>
2020-03-03 19:03:03 +0700

    update graphql api

--- src/Mix.Cms.Api.GraphQL/Controllers/GraphqlController.cs
+++ src/Mix.Cms.Api.GraphQL/Controllers/GraphqlController.cs
@@ -1,17 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
 using System.Threading.Tasks;
-//using EntityGraphQL;
-//using EntityGraphQL.Schema;
 using GraphQL;
-//using GraphQL.NewtonsoftJson;
 using GraphQL.Types;
-using Api.Database;
-using Api.Graphql;
 using Microsoft.AspNetCore.Mvc;
 using Mix.Cms.Lib.Models.Cms;
+using Mix.Cms.Api.GraphQL.Infrastructure.Models;
 
 namespace Mix.Cms.Api.GraphQL.Controllers
 {
@@ -19,86 +11,31 @@ namespace Mix.Cms.Api.GraphQL.Controllers
     [ApiController]
     public class GraphqlController : ControllerBase
     {
-        [HttpPost]
-        public async Task<ActionResult> Post([FromBody] GraphQLQuery query)
+        private static readonly Schema graphQLSchema;
+        static GraphqlController()
         {
-            var schema = new MySchema();
-            var inputs = query.Variables.ToInputs();
-
-            var result = await new DocumentExecuter().ExecuteAsync(_ =>
-            {
-                _.Schema = schema.GraphQLSchema;
-                _.Query = query.Query;
-                _.OperationName = query.OperationName;
-                _.Inputs = inputs;
-            });
-
+            var dbContext = new MixCmsContext();
+            var tableNameLookup = new TableNameLookup();
+            var metaDatabase = new DatabaseMetadata(dbContext, tableNameLookup);
+            var schema = new Schema { Query = new Mix.Cms.Api.GraphQL.Infrastructure.GraphQLQuery(dbContext, metaDatabase, tableNameLookup) };
+            schema.Initialize();
+            graphQLSchema = schema;
+        }
+        [HttpPost]
+        public async Task<ActionResult> Get([FromBody] QueryRequest query)
+        {
+            var result = await new DocumentExecuter().ExecuteAsync(
+                new ExecutionOptions()
+                {
+                    Schema = graphQLSchema,
+                    Query = query.Query
+                }
+            ).ConfigureAwait(false);
             if (result.Errors?.Count > 0)
             {
                 return BadRequest(result.Errors);
             }
-
             return Ok(result);
         }
-
-        //private readonly Schema graphQLSchema;
-        //public GraphqlController()
-        //{
-        //    var dbContext = new MixCmsContext();
-        //    var tableNameLookup = new TableNameLookup();
-        //    var metaDatabase = new DatabaseMetadata(dbContext, tableNameLookup);
-        //    var schema = new Schema { Query = new Mix.Cms.Api.GraphQL.Infrastructure.GraphQLQuery(dbContext, metaDatabase, tableNameLookup) };
-        //    schema.Initialize();
-        //    graphQLSchema = schema;
-        //}
-        //[HttpPost]
-        //public async Task<ActionResult> Get([FromBody] GraphQLQuery query)
-        //{
-        //    var result = await new DocumentExecuter().ExecuteAsync(
-        //        new ExecutionOptions()
-        //        {
-        //            Schema = graphQLSchema,
-        //            Query = query.Query
-        //        }
-        //    ).ConfigureAwait(false); 
-        //    if (result.Errors?.Count > 0)
-        //    {
-        //        return BadRequest(result.Errors);
-        //    }
-        //    return Ok(result);
-        //    //var json = new DocumentWriter(indent: true).Write(result.Data); return json;
-        //}
-
-        //private readonly MappedSchemaProvider<MixCmsContext> _schemaprovider;
-
-        //public GraphqlController(MappedSchemaProvider<MixCmsContext> schemaprovider)
-        //{
-        //    this._schemaprovider = schemaprovider;
-        //}
-
-        //[HttpPost]
-        //public ActionResult post([FromBody]QueryRequest query)
-        //{
-        //    try
-        //    {
-        //        using (MixCmsContext dbcontext = new MixCmsContext())
-        //        {
-        //            var results = dbcontext.QueryObject(query, _schemaprovider);
-        //            // gql compile errors show up in results.errors
-        //            if (results.Errors.Count == 0)
-        //            {
-        //                return Ok(results);
-        //            }
-        //            else
-        //            {
-        //                return BadRequest(results.Errors);
-        //            }
-        //        }
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        return BadRequest(ex);
-        //    }
-        //}
     }
 }
--- src/Mix.Cms.Api.GraphQL/Infrastructure/Resolvers/NameFieldResolver.cs
+++ src/Mix.Cms.Api.GraphQL/Infrastructure/Resolvers/NameFieldResolver.cs
@@ -18,10 +18,10 @@ namespace Mix.Cms.Api.GraphQL.Infrastructure
             }
             var name = Char.ToUpperInvariant(context.FieldAst.Name[0]) + context.FieldAst.Name.Substring(1);
             var value = GetPropValue(source, name);
-            if (value == null)
-            {
-                throw new InvalidOperationException($"Expected to find property {context.FieldAst.Name} on {context.Source.GetType().Name} but it does not exist.");
-            }
+            //if (value == null)
+            //{
+            //    throw new InvalidOperationException($"Expected to find property {context.FieldAst.Name} on {context.Source.GetType().Name} but it does not exist.");
+            //}
             return value;
         }
         private static object GetPropValue(object src, string propName)
