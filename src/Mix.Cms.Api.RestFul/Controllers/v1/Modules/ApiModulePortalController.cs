8843b687f14225e669f2838b53cb2aea17f6555b
Hoang Nguyen <nhathoang989@gmail.com>
2020-05-17 16:47:24 +0700

    clean src

---
+++ src/Mix.Cms.Api.RestFul/Controllers/v1/Modules/ApiModulePortalController.cs
@@ -0,0 +1,55 @@
+﻿// Licensed to the Mixcore Foundation under one or more agreements.
+// The Mixcore Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Mix.Cms.Lib;
+using Mix.Cms.Lib.Controllers;
+using Mix.Cms.Lib.Models.Cms;
+using Mix.Cms.Lib.ViewModels.MixModules;
+using Mix.Domain.Core.ViewModels;
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Mix.Cms.Api.RestFul.Controllers.v1
+{
+    [Produces("application/json")]
+    [Route("api/v1/rest/{culture}/module/portal")]
+    public class ApiModuleController :
+        BaseRestApiController<MixCmsContext, MixModule, UpdateViewModel>
+    {
+
+        // GET: api/s
+        [HttpGet]
+        public override async Task<ActionResult<PaginationModel<UpdateViewModel>>> Get()
+        {
+            bool isStatus = Enum.TryParse(Request.Query["status"], out MixEnums.MixContentStatus status);
+            bool isFromDate = DateTime.TryParse(Request.Query["fromDate"], out DateTime fromDate);
+            bool isToDate = DateTime.TryParse(Request.Query["toDate"], out DateTime toDate);
+            string keyword = Request.Query["keyword"];
+            Expression<Func<MixModule, bool>> predicate = model =>
+                model.Specificulture == _lang
+                && (!isStatus || model.Status == status.ToString())
+                && (!isFromDate || model.CreatedDateTime >= fromDate)
+                && (!isToDate || model.CreatedDateTime <= toDate)
+                && (string.IsNullOrEmpty(keyword)
+                 || (EF.Functions.Like(model.Title, $"%{keyword}%"))
+                 || (EF.Functions.Like(model.Description, $"%{keyword}%"))
+                 || (EF.Functions.Like(model.Name, $"%{keyword}%"))
+                 );
+            var getData = await base.GetListAsync(predicate);
+            if (getData.IsSucceed)
+            {
+                return getData.Data;
+            }
+            else
+            {
+                return BadRequest(getData.Errors);
+            }
+        }
+    }
+
+}
