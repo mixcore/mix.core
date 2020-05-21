1598e975025bbc0a5597c22e778a685e9a11a540
Hoang Nguyen <nhathoang989@gmail.com>
2020-05-21 23:49:32 +0700

    fix builds

---
+++ src/Mix.Heart.NetCore/Attributes/GeneratedControllerAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mix.Heart.NetCore.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+    public class GeneratedControllerAttribute : Attribute
+    {
+        public GeneratedControllerAttribute(string route)
+        {
+            Route = route;
+        }
+
+        public string Route { get; set; }
+    }
+}
