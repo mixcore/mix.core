5ddf8532c53a26363df3043cad83cd3eb6545e9d
Hoang Nguyen <nhathoang989@gmail.com>
2020-09-16 03:21:34 +0700

    Handle migration for MSSQL and MySQL

--- src/Mix.Cms.Lib/Models/Cms/_MySqlMixCmsContext.cs
+++ src/Mix.Cms.Lib/Models/Cms/_MySqlMixCmsContext.cs
@@ -6,39 +6,8 @@ using System;
 
 namespace Mix.Cms.Lib.Models.Cms
 {
-    public partial class MySqlMixCmsContext : DbContext
+    public partial class MySqlMixCmsContext : MixCmsContext
     {
-        public virtual DbSet<MixAttributeField> MixAttributeField { get; set; }
-        public virtual DbSet<MixAttributeSet> MixAttributeSet { get; set; }
-        public virtual DbSet<MixAttributeSetData> MixAttributeSetData { get; set; }
-        public virtual DbSet<MixAttributeSetReference> MixAttributeSetReference { get; set; }
-        public virtual DbSet<MixAttributeSetValue> MixAttributeSetValue { get; set; }
-        public virtual DbSet<MixCache> MixCache { get; set; }
-        public virtual DbSet<MixCmsUser> MixCmsUser { get; set; }
-        public virtual DbSet<MixConfiguration> MixConfiguration { get; set; }
-        public virtual DbSet<MixCulture> MixCulture { get; set; }
-        public virtual DbSet<MixFile> MixFile { get; set; }
-        public virtual DbSet<MixLanguage> MixLanguage { get; set; }
-        public virtual DbSet<MixMedia> MixMedia { get; set; }
-        public virtual DbSet<MixModule> MixModule { get; set; }
-        public virtual DbSet<MixModuleData> MixModuleData { get; set; }
-        public virtual DbSet<MixModulePost> MixModulePost { get; set; }
-        public virtual DbSet<MixPage> MixPage { get; set; }
-        public virtual DbSet<MixPageModule> MixPageModule { get; set; }
-        public virtual DbSet<MixPagePost> MixPagePost { get; set; }
-        public virtual DbSet<MixPortalPage> MixPortalPage { get; set; }
-        public virtual DbSet<MixPortalPageNavigation> MixPortalPageNavigation { get; set; }
-        public virtual DbSet<MixPortalPageRole> MixPortalPageRole { get; set; }
-        public virtual DbSet<MixPost> MixPost { get; set; }
-        public virtual DbSet<MixPostMedia> MixPostMedia { get; set; }
-        public virtual DbSet<MixPostModule> MixPostModule { get; set; }
-        public virtual DbSet<MixRelatedAttributeData> MixRelatedAttributeData { get; set; }
-        public virtual DbSet<MixRelatedAttributeSet> MixRelatedAttributeSet { get; set; }
-        public virtual DbSet<MixRelatedPost> MixRelatedPost { get; set; }
-        public virtual DbSet<MixTemplate> MixTemplate { get; set; }
-        public virtual DbSet<MixTheme> MixTheme { get; set; }
-        public virtual DbSet<MixUrlAlias> MixUrlAlias { get; set; }
-
         /// <summary>
         /// Initializes a new instance of the <see cref="ApplicationDbContext" /> class.
         /// </summary>
