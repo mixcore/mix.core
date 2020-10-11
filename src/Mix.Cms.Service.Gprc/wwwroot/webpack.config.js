b8b18c864295b7c506252e20709c96c603da2bc0
Hoang Nguyen <nhathoang989@gmail.com>
2020-10-11 14:20:05 +0700

    remove unneed projects

--- src/Mix.Cms.Service.Gprc/wwwroot/webpack.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-module.exports = {
-  entry: './Scripts/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'mix-gprc.min.js'
-  }
-};
