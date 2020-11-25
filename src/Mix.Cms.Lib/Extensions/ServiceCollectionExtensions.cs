073d2285e2047e926ea5460033be95b42788d14d
Hoang Nguyen <nhathoang989@gmail.com>
2020-11-25 13:15:54 +0700

    fix auto generate rest api

--- src/Mix.Cms.Lib/Extensions/ServiceCollectionExtensions.cs
+++ src/Mix.Cms.Lib/Extensions/ServiceCollectionExtensions.cs
@@ -15,7 +15,8 @@ namespace Mix.Cms.Lib.Extensions
 
         public static IServiceCollection AddGenerateApis(this IServiceCollection services)
         {
-            services.AddGeneratedRestApi(Assembly.GetExecutingAssembly(), typeof(BaseRestApiController<,,,>));
+            services.AddGeneratedRestApi(Assembly.GetExecutingAssembly(), typeof(BaseRestApiController<,,>));
+            //services.AddGeneratedRestApi(Assembly.GetExecutingAssembly(), typeof(BaseRestApiController<,,,>));
             //services.AddGeneratedRestApi(Assembly.GetExecutingAssembly(), typeof(BaseRestApiController<,,,,>));
 
             services.AddSignalR();
