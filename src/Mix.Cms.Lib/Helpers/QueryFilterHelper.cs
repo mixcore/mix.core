46d66add81907e4b4416ef445679e6477d95ab09
Hoang Nguyen <nhathoang989@gmail.com>
2021-01-11 08:05:39 +0700

    fix query filter naming

--- src/Mix.Cms.Lib/Helpers/QueryFilterHelper.cs
+++ src/Mix.Cms.Lib/Helpers/QueryFilterHelper.cs
@@ -2,6 +2,7 @@
 using System.Linq.Expressions;
 using Microsoft.EntityFrameworkCore;
 using Mix.Cms.Lib.Enums;
+using Mix.Cms.Lib.Extensions;
 using Mix.Cms.Lib.Models;
 using Mix.Cms.Lib.Models.Cms;
 using Newtonsoft.Json.Linq;
@@ -16,56 +17,23 @@ namespace Mix.Cms.Lib.Helpers
                 .AddExpression
                 (
                     ExpressionModel.Create(MixLogicalOperatorKind.And)
-                        .AddFunction(FunctionModel.Create(MixCompareOperatorKind.Eq, "ho_va_ten", "Tran Nhat Duy"))
-                        .AddFunction(FunctionModel.Create(MixCompareOperatorKind.Eq, "status", "open"))
+                        .AddFunction(FunctionModel.Create(MixCompareOperatorKind.Equal, "ho_va_ten", "Tran Nhat Duy"))
+                        .AddFunction(FunctionModel.Create(MixCompareOperatorKind.Equal, "status", "open"))
                 ).AddExpression
                 (
                     ExpressionModel.Create(MixLogicalOperatorKind.And)
                     .AddExpression
                     (
                         ExpressionModel.Create(MixLogicalOperatorKind.Or)
-                            .AddFunction(FunctionModel.Create(MixCompareOperatorKind.Eq, "date", "2020/12/10", "2020/12/30"))
-                            .AddFunction(FunctionModel.Create(MixCompareOperatorKind.Eq, "address", "Nguyen thi Minh Khai"))
+                            .AddFunction(FunctionModel.Create(MixCompareOperatorKind.Equal, "date", "2020/12/10", "2020/12/30"))
+                            .AddFunction(FunctionModel.Create(MixCompareOperatorKind.Equal, "address", "Nguyen thi Minh Khai"))
                     ).AddExpression
                     (
                         ExpressionModel.Create(MixLogicalOperatorKind.And)
-                            .AddFunction(FunctionModel.Create(MixCompareOperatorKind.Gt, "age", "18"))
-                            .AddFunction(FunctionModel.Create(MixCompareOperatorKind.Eq, "gender", "male"))
+                            .AddFunction(FunctionModel.Create(MixCompareOperatorKind.GreaterThan, "age", "18"))
+                            .AddFunction(FunctionModel.Create(MixCompareOperatorKind.Equal, "gender", "male"))
                     )
                 );
-        }
-
-
-
-        /// <summary>
-        /// Combine Two Expressions by Type
-        /// </summary>
-        /// <param name="type"></param>
-        /// <param name="expr1"></param>
-        /// <param name="expr2"></param>
-        /// <param name="parameter"></param>
-        /// <returns></returns>
-        private static Expression<Func<MixAttributeSetValue, bool>> CombineExpressionByType(MixLogicalOperatorKind type,
-            Expression<Func<MixAttributeSetValue, bool>> expr1,
-            Expression<Func<MixAttributeSetValue, bool>> expr2, ParameterExpression parameter)
-        {
-            Expression left = null;
-            if (expr1 != null)
-            {
-                var leftVisitor = new ReplaceExpressionVisitor(expr1.Parameters[0], parameter);
-                left = leftVisitor.Visit(expr1.Body);
-            }
-
-            var rightVisitor = new ReplaceExpressionVisitor(expr2.Parameters[0], parameter);
-            var right = rightVisitor.Visit(expr2.Body);
-            return type switch
-            {
-                MixLogicalOperatorKind.And => expr1 != null ? Expression.Lambda<Func<MixAttributeSetValue, bool>>(Expression.AndAlso(left, right), parameter) :
-                                        Expression.Lambda<Func<MixAttributeSetValue, bool>>(right, parameter),
-                MixLogicalOperatorKind.Or => expr1 != null ? Expression.Lambda<Func<MixAttributeSetValue, bool>>(Expression.OrElse(left, right), parameter) :
-                                        Expression.Lambda<Func<MixAttributeSetValue, bool>>(right, parameter),
-                _ => null,
-            };
         }
 
         public static Expression<Func<MixAttributeSetValue, bool>> CreateExpression(
@@ -87,26 +55,25 @@ namespace Mix.Cms.Lib.Helpers
             return null;
         }
 
+       
+
         private static Expression<Func<MixAttributeSetValue, bool>> GetExpression(
          ExpressionModel expressionModel)
         {
-            MixLogicalOperatorKind expressionType = expressionModel.ExpressionType;
-
-            var parameter = Expression.Parameter(typeof(MixAttributeSetValue), "m");
             Expression<Func<MixAttributeSetValue, bool>> root = null;
             try
             {
                 expressionModel?.Functions?.ForEach(function =>
                 {
-                    root = CombineExpressionByType(expressionType,
-                        root, GetFunction(function), parameter);
+                    root = CombineExpressionByType(expressionModel.ExpressionType,
+                        root, GetFunction(function));
                 });
 
 
                 expressionModel?.Expressions?.ForEach(expression =>
                 {
-                    root = CombineExpressionByType(expressionType,
-                            root, GetExpression(expression), parameter);
+                    root = CombineExpressionByType(expressionModel.ExpressionType,
+                            root, GetExpression(expression));
                 });
 
                 return root;
@@ -114,47 +81,66 @@ namespace Mix.Cms.Lib.Helpers
             catch (Exception ex)
             {
                 Console.Write(ex.ToString());
+                return null;
             }
-
-            return null;
         }
 
+        /// <summary>
+        /// Combine Two Expressions by Type
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="expr1"></param>
+        /// <param name="expr2"></param>
+        /// <param name="parameter"></param>
+        /// <returns></returns>
+        private static Expression<Func<MixAttributeSetValue, bool>> CombineExpressionByType(
+            MixLogicalOperatorKind type,
+            Expression<Func<MixAttributeSetValue, bool>> expr1,
+            Expression<Func<MixAttributeSetValue, bool>> expr2)
+        {
+            return type switch
+            {
+                MixLogicalOperatorKind.And => expr1.AndAlsoIf(expr1 != null, expr2),
+                MixLogicalOperatorKind.Or => expr1.OrIf(expr1 != null, expr2),
+                _ => expr2,
+            };
+        }
         private static Expression<Func<MixAttributeSetValue, bool>> GetFunction(FunctionModel functionModel)
         {
             return functionModel.Rule switch
             {
-                MixCompareOperatorKind.Eq => m => m.AttributeFieldName == functionModel.FieldName
+                MixCompareOperatorKind.Equal => m => m.AttributeFieldName == functionModel.FieldName
                 && m.StringValue == functionModel.Value,
 
-                MixCompareOperatorKind.Neq => m => m.AttributeFieldName == functionModel.FieldName
+                MixCompareOperatorKind.NotEqual => m => m.AttributeFieldName == functionModel.FieldName
                 && m.StringValue != functionModel.Value,
 
-                MixCompareOperatorKind.Ct => m => m.AttributeFieldName == functionModel.FieldName
+                MixCompareOperatorKind.Contain => m => m.AttributeFieldName == functionModel.FieldName
                 && (EF.Functions.Like(m.StringValue, buildLikeString(functionModel.Value))),
 
-                MixCompareOperatorKind.Nct => m => m.AttributeFieldName == functionModel.FieldName
+                MixCompareOperatorKind.NotContain => m => m.AttributeFieldName == functionModel.FieldName
                 && !(EF.Functions.Like(m.StringValue, buildLikeString(functionModel.Value))),
 
-                MixCompareOperatorKind.Ra => m => m.AttributeFieldName == functionModel.FieldName
+                MixCompareOperatorKind.InRange => m => m.AttributeFieldName == functionModel.FieldName
                 && (string.Compare(m.StringValue, functionModel.MinValue) > 0
                 && string.Compare(m.StringValue, functionModel.MaxValue) < 0),
 
-                MixCompareOperatorKind.Nra => m => m.AttributeFieldName == functionModel.FieldName
+                MixCompareOperatorKind.NotInRange => m => m.AttributeFieldName == functionModel.FieldName
                 && (string.Compare(m.StringValue, functionModel.MinValue) < 0
                 || string.Compare(m.StringValue, functionModel.MaxValue) > 0),
 
-                MixCompareOperatorKind.Gte => m => m.AttributeFieldName == functionModel.FieldName
+                MixCompareOperatorKind.GreaterThanOrEqual => m => m.AttributeFieldName == functionModel.FieldName
                 && (string.Compare(m.StringValue, functionModel.Value) > 0
                 || m.StringValue == functionModel.Value),
 
-                MixCompareOperatorKind.Gt => m => m.AttributeFieldName == functionModel.FieldName
+                MixCompareOperatorKind.GreaterThan => m => m.AttributeFieldName == functionModel.FieldName
                 && string.Compare(m.StringValue, functionModel.Value) > 0,
 
-                MixCompareOperatorKind.Lte => m => m.AttributeFieldName == functionModel.FieldName
+                MixCompareOperatorKind.LessThanOrEqual => m => m.AttributeFieldName == functionModel.FieldName
                 && (string.Compare(m.StringValue, functionModel.Value) < 0
                 || m.StringValue == functionModel.Value),
 
-                MixCompareOperatorKind.Lt => m => m.AttributeFieldName == functionModel.FieldName
+                MixCompareOperatorKind.LessThan => m => m.AttributeFieldName == functionModel.FieldName
                 && string.Compare(m.StringValue, functionModel.Value) < 0,
 
                 _ => null,
@@ -164,26 +150,6 @@ namespace Mix.Cms.Lib.Helpers
         private static string buildLikeString(string stringValue)
         {
             return $"%{stringValue}%";
-        }
-    }
-
-
-    public class ReplaceExpressionVisitor : ExpressionVisitor
-    {
-        private readonly Expression _oldValue;
-        private readonly Expression _newValue;
-
-        public ReplaceExpressionVisitor(Expression oldValue, Expression newValue)
-        {
-            _oldValue = oldValue;
-            _newValue = newValue;
-        }
-
-        public override Expression Visit(Expression node)
-        {
-            if (node == _oldValue)
-                return _newValue;
-            return base.Visit(node);
         }
     }
 }
