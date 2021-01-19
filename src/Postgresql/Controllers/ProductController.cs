3b945b78d1dfc95c2ff6b350022bdbfd9a619371
phat <tanphat1991996@gmail.com>
2021-01-19 09:01:38 +0700

    example

---
+++ src/Postgresql/Controllers/ProductController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Postgresql.Infrastructures;
+using Postgresql.Infrastructures.Domain;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Postgresql.Controllers
+{
+    [Route("api/products")]
+    public class ProductController : ControllerBase
+    {
+        [HttpPost]
+        public async Task Create()
+        {
+            var newProduct = new Product
+            {
+                Id = Guid.NewGuid(),
+                Name = "1234567890",
+                CreatedDate = DateTime.Now
+            };
+
+            using (var dbContext = new PostgresDbContext())
+            {
+                dbContext.Add<Product>(newProduct);
+                await dbContext.SaveChangesAsync();
+            }
+        }
+
+        [HttpGet]
+        public async Task<List<Product>> Get()
+        {
+            var products = new List<Product>();
+
+            using (var dbContext = new PostgresDbContext())
+            {
+                products.AddRange(await dbContext.Products.ToListAsync());
+            }
+
+            return products;
+        }
+    }
+}
---
+++ src/Postgresql/Infrastructures/NpgsqlConfigurationExtension.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Postgresql.Infrastructures
+{
+    public static class NpgsqlConfigurationExtension
+    {
+        public static void AddPostgresDbContext(this IServiceCollection service, string connectionStrings)
+        {
+            service.AddDbContext<PostgresDbContext>(options => options.UseNpgsql(connectionStrings));
+        }
+    }
+}
