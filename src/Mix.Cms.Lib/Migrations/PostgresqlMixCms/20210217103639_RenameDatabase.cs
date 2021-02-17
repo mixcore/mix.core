9e594231de7e0b5735e1d844eaac0856c7ccb975
Hoang Nguyen Nhat <hoang.nguyen@orientsoftware.net>
2021-02-17 18:53:48 +0700

    Fix update database

--- src/Mix.Cms.Lib/Migrations/PostgresqlMixCms/20210217103639_RenameDatabase.cs
+++ src/Mix.Cms.Lib/Migrations/PostgresqlMixCms/20210217103639_RenameDatabase.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore.Migrations;
-using Mix.Cms.Lib.Models.Cms;
-using Microsoft.EntityFrameworkCore;
-using System.Data;
+using Mix.Cms.Lib.Constants;
+using Mix.Cms.Lib.Services;
 
 namespace Mix.Cms.Lib.Migrations.PostgresqlMixCms
 {
@@ -9,71 +8,37 @@ namespace Mix.Cms.Lib.Migrations.PostgresqlMixCms
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            using (var context = new MixCmsContext())
+            if (MixService.GetConfig<string>(MixConfigurations.CONST_MIXCORE_VERSION) == "1.0.0")
             {
-                if (TableExists("mix_attribute_set"))
-                {
-                    migrationBuilder.DropTable("mix_related_attribute_set");
-                    migrationBuilder.DropTable("mix_attribute_set_reference");
+                migrationBuilder.DropTable("mix_related_attribute_set");
+                migrationBuilder.DropTable("mix_attribute_set_reference");
 
-                    migrationBuilder.RenameTable("mix_related_post", null, "mix_post_association");
-                    migrationBuilder.RenameTable("mix_attribute_set", null, "mix_database");
-                    migrationBuilder.RenameTable("mix_related_attribute_data", null, "mix_database_data_association");
-                    migrationBuilder.RenameTable("mix_attribute_set_value", null, "mix_database_data_value");
-                    migrationBuilder.RenameTable("mix_attribute_set_data", null, "mix_database_data");
+                migrationBuilder.RenameTable("mix_related_post", null, "mix_post_association");
+                migrationBuilder.RenameTable("mix_attribute_set", null, "mix_database");
+                migrationBuilder.RenameTable("mix_related_attribute_data", null, "mix_database_data_association");
+                migrationBuilder.RenameTable("mix_attribute_set_value", null, "mix_database_data_value");
+                migrationBuilder.RenameTable("mix_attribute_set_data", null, "mix_database_data");
 
-                    migrationBuilder.RenameColumn("AttributeFieldId", "mix_database_data_value", "MixDatabaseColumnId");
-                    migrationBuilder.RenameColumn("AttributeFieldName", "mix_database_data_value", "MixDatabaseColumnName");
-                    migrationBuilder.RenameColumn("AttributeSetName", "mix_database_data_value", "MixDatabaseName");
+                migrationBuilder.RenameColumn("AttributeFieldId", "mix_database_data_value", "MixDatabaseColumnId");
+                migrationBuilder.RenameColumn("AttributeFieldName", "mix_database_data_value", "MixDatabaseColumnName");
+                migrationBuilder.RenameColumn("AttributeSetName", "mix_database_data_value", "MixDatabaseName");
 
 
-                    migrationBuilder.RenameColumn("AttributeSetId", "mix_database_data_association", "MixDatabaseId");
-                    migrationBuilder.RenameColumn("AttributeSetName", "mix_database_data_association", "MixDatabaseName");
+                migrationBuilder.RenameColumn("AttributeSetId", "mix_database_data_association", "MixDatabaseId");
+                migrationBuilder.RenameColumn("AttributeSetName", "mix_database_data_association", "MixDatabaseName");
 
-                    migrationBuilder.RenameColumn("AttributeSetId", "mix_database_data", "MixDatabaseId");
-                    migrationBuilder.RenameColumn("AttributeSetName", "mix_database_data", "MixDatabaseName");
+                migrationBuilder.RenameColumn("AttributeSetId", "mix_database_data", "MixDatabaseId");
+                migrationBuilder.RenameColumn("AttributeSetName", "mix_database_data", "MixDatabaseName");
 
-                    migrationBuilder.RenameTable("mix_attribute_field", null, "mix_database_column");
-                    migrationBuilder.RenameColumn("AttributeSetId", "mix_database_column", "MixDatabaseId");
-                    migrationBuilder.RenameColumn("AttributeSetName", "mix_database_column", "MixDatabaseName");
-                }
+                migrationBuilder.RenameTable("mix_attribute_field", null, "mix_database_column");
+                migrationBuilder.RenameColumn("AttributeSetId", "mix_database_column", "MixDatabaseId");
+                migrationBuilder.RenameColumn("AttributeSetName", "mix_database_column", "MixDatabaseName");
             }
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
 
-        }
-
-        private bool TableExists(string tableName, string schema = "dbo")
-        {
-            using (var context = new MixCmsContext())
-            {
-                var connection = context.Database.GetDbConnection();
-
-                if (connection.State.Equals(ConnectionState.Closed))
-                    connection.Open();
-
-                using (var command = connection.CreateCommand())
-                {
-                    command.CommandText = @"
-            SELECT 1 FROM INFORMATION_SCHEMA.TABLES 
-            WHERE TABLE_SCHEMA = @Schema
-            AND TABLE_NAME = @TableName";
-
-                    var schemaParam = command.CreateParameter();
-                    schemaParam.ParameterName = "@Schema";
-                    schemaParam.Value = schema;
-                    command.Parameters.Add(schemaParam);
-
-                    var tableNameParam = command.CreateParameter();
-                    tableNameParam.ParameterName = "@TableName";
-                    tableNameParam.Value = tableName;
-                    command.Parameters.Add(tableNameParam);
-
-                    return command.ExecuteScalar() != null;
-                }
-            }
         }
     }
 }
