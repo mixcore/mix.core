578b4086fdaee7428c269c581610c0b406053956
Smilefounder <i.love.to.smile.around@gmail.com>
2021-02-27 15:45:29 +0700

    Add Mix.Theme.Blog proj

---
+++ src/Mix.Theme.Blog/Posts.cs
@@ -0,0 +1,38 @@
+﻿using Mix.Cms;
+using Mix.Identity.Models;
+using Mix.Identity.Models.AccountViewModels;
+using Mix.Identity.Models.ManageViewModels;
+using Microsoft.AspNetCore.Identity;
+using System.Globalization;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.Localization;
+using Mix.Domain.Core.ViewModels;
+using Mix.Cms.Lib.ViewModels;
+using Mix.Cms.Lib;
+using Mix.Cms.Lib.Services;
+using Mix.Cms.Lib.Enums;
+using Mix.Heart.Enums;
+using Mix.Cms.Lib.Constants;
+using Newtonsoft.Json.Linq;
+
+using MixModules = Mix.Cms.Lib.ViewModels.MixModules;
+using MixPosts = Mix.Cms.Lib.ViewModels.MixPosts;
+using MixPages = Mix.Cms.Lib.ViewModels.MixPages;
+using MixDatas = Mix.Cms.Lib.ViewModels.MixDatabaseDatas;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Mix.Theme.Blog
+{
+    class Posts
+    {
+        public async Task<dynamic> MyPosts(string culture, IUrlHelper Url)
+        {
+            return await MixPosts.ReadMvcViewModel.Repository.GetModelListByAsync(
+                m => m.Status == MixContentStatus.Published,
+                "CreatedDateTime",
+                MixHeartEnums.DisplayDirection.Desc, 15, 0);
+
+        }
+    }
+}
