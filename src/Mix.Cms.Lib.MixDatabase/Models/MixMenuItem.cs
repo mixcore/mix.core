9eb619e9c4260c4537e7a7f59b8f6a1c351c4318
Hoang Nguyen Nhat <hoang.nguyen@orientsoftware.net>
2021-04-18 14:51:52 +0700

    fix bugs

---
+++ src/Mix.Cms.Lib.MixDatabase/Models/MixMenuItem.cs
@@ -0,0 +1,85 @@
+﻿using Mix.Cms.Lib.Constants;
+using Mix.Cms.Lib.Enums;
+using Mix.Cms.Lib.MixDatabase.Attributes;
+using Mix.Cms.Lib.Services;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+
+
+namespace Mix.Cms.Lib.MixDatabase.Models
+{
+    [MixDatabase("sys_menu_item")]
+    public class MixMenuItem
+    {
+        [JsonIgnore]
+        [JsonProperty("obj")]
+        public JObject Obj { get; set; }
+
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("specificulture")]
+        public string Specificulture { get; set; }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("uri")]
+        public string Uri { get; set; }
+
+        [JsonProperty("href")]
+        public string Href
+        {
+            get
+            {
+                var domain = MixService.GetConfig<string>(MixAppSettingKeywords.Domain);
+                return Uri != null && Uri.Contains(domain) ? Uri : $"{domain}{Uri}";
+            }
+        }
+
+        [JsonProperty("icon")]
+        public string Icon { get; set; }
+
+        [JsonProperty("type")]
+        public MixMenuItemType Type { get; set; }
+
+        [JsonProperty("target")]
+        public string Target { get; set; }
+
+        [JsonProperty("classes")]
+        public string Classes { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("target_id")]
+        public string TargetId { get; set; }
+
+        [JsonProperty("isActive")]
+        public bool IsActive { get; set; }
+
+        [JsonProperty("menu_items")]
+        public List<MixMenuItem> MenuItems { get; set; }
+
+        public T Property<T>(string fieldName)
+        {
+            if (Obj != null)
+            {
+                var field = Obj.GetValue(fieldName);
+                if (field != null)
+                {
+                    return field.Value<T>();
+                }
+                else
+                {
+                    return default;
+                }
+            }
+            else
+            {
+                return default;
+            }
+        }
+    }
+}
