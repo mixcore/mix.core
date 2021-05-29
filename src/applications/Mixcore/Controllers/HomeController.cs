1038d24224d7d2ef63d76ab10a16e534c7f34e47
Hoang Nguyen Nhat <hoang.nguyen@orientsoftware.net>
2021-05-30 02:19:49 +0700

    Cleanup

--- src/applications/Mixcore/Controllers/HomeController.cs
+++ src/applications/Mixcore/Controllers/HomeController.cs
@@ -1,13 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using mixcore.Models;
+using Mixcore.Models;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace mixcore.Controllers
+namespace Mixcore.Controllers
 {
     public class HomeController : Controller
     {
--- src/applications/Mixcore/Models/ErrorViewModel.cs
+++ src/applications/Mixcore/Models/ErrorViewModel.cs
@@ -1,6 +1,6 @@
 using System;
 
-namespace mixcore.Models
+namespace Mixcore.Models
 {
     public class ErrorViewModel
     {
