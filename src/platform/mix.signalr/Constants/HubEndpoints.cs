a55746152f5be9ae1b942e1d9895eca2a8bfe033
Hoang Nguyen Nhat <hoang.nguyen@orientsoftware.net>
2021-07-04 10:21:37 +0700

    clean up

---
+++ src/platform/mix.signalr/Constants/HubEndpoints.cs
@@ -0,0 +1,8 @@
+﻿namespace Mix.SignalR.Constants
+{
+    public class HubEndpoints
+    {
+        public const string PortalHub = "/portalhub";
+        public const string EditFileHub = "/editFileHub";
+    }
+}
