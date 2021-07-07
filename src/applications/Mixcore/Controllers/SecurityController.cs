323653fdc34947d3e96198e00ed99bbadbca393f
Hoang Nguyen Nhat <hoang.nguyen@orientsoftware.net>
2021-07-07 22:35:46 +0700

    cleanup security app

---
+++ src/applications/Mixcore/Controllers/SecurityController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mix.Lib.Controllers;
+using Mix.Shared.Services;
+using Mix.Lib.Services;
+
+namespace Mixcore.Controllers
+{
+    public class SecurityController : MixControllerBase
+    {
+        public SecurityController(MixAppSettingService appSettingService, MixService mixService) 
+            : base(appSettingService, mixService)
+        {
+        }
+
+        [HttpGet]
+        [Route("security/{page}")]
+        public IActionResult Index(string page)
+        {
+            if (isValid)
+            {
+                return View();
+            }
+            else
+            {
+                return Redirect(_redirectUrl);
+            }
+        }
+    }
+}
