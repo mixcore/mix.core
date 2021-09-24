658f4bf75312dc2781e234eff79d7d0290c8a27b
Hoang Nguyen Nhat <hoang.nguyen@orientsoftware.net>
2021-09-25 03:16:43 +0700

    Add Module Endpoint Service

---
+++ src/platform/mix.shared/Constants/MixModuleNames.cs
@@ -0,0 +1,14 @@
+﻿namespace Mix.Shared.Constants
+{
+    public class MixModuleNames
+    {
+        public const string Account = "Account";
+        public const string Common = "Common";
+        public const string Portal = "Portal";
+        public const string Theme = "Theme";
+        public const string Mixcore = "Mixcore";
+        public const string Messenger = "Messenger";
+        public const string Scheduler = "Scheduler";
+        public const string Grpc = "Grpc";
+    }
+}
