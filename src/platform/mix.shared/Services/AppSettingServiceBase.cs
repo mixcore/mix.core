04398413e86f536837e7b8f2662b79fd767d7090
Hoang Nguyen Nhat <hoang.nguyen@orientsoftware.net>
2021-10-30 02:27:11 +0700

    Add appsetting service

---
+++ src/platform/mix.shared/Services/AppSettingServiceBase.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Extensions.Configuration;
+using Mix.Heart.Exceptions;
+using Mix.Heart.Services;
+using Mix.Shared.Constants;
+using Mix.Shared.Enums;
+using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+
+namespace Mix.Shared.Services
+{
+    public abstract class AppSettingServiceBase<T>
+    {
+        protected string filePath;
+        protected string _sectionName;
+        public T AppSettings { get; set; }
+        protected string FilePath { get => filePath; set => filePath = value; }
+
+        protected readonly FileSystemWatcher watcher = new();
+        protected readonly IConfiguration _configuration;
+
+        public AppSettingServiceBase(IConfiguration configuration, MixAppSettingsSection section, string filePath)
+        {
+            _configuration = configuration;
+            _sectionName = section.ToString();
+            FilePath = filePath;
+            LoadAppSettings();
+        }
+
+        public bool SaveSettings()
+        {
+            var settings = MixFileService.Instance.GetFile(FilePath, MixFileExtensions.Json, string.Empty, true, "{}");
+            if (settings != null)
+            {
+                settings.Content = new JObject(new JProperty(_sectionName, JObject.FromObject(AppSettings))).ToString();
+                return MixFileService.Instance.SaveFile(settings);
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        protected virtual void LoadAppSettings()
+        {
+            try
+            {
+                var settings = _configuration.GetSection(_sectionName);
+                BindAppSettings(settings);
+                Console.WriteLine(AppSettings);
+            }
+            catch(Exception ex)
+            {
+                throw new MixException($"Cannot load config section {_sectionName}: {ex.Message}");
+            }
+        }
+
+        protected abstract void BindAppSettings(IConfigurationSection settings);
+    }
+}
---
+++ src/platform/mix.shared/Services/MixHeartConfigService.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Extensions.Configuration;
+using Mix.Heart.Constants;
+using Mix.Heart.Enums;
+using Mix.Heart.Exceptions;
+using Mix.Heart.Model;
+using Mix.Shared.Constants;
+using Mix.Shared.Enums;
+using System;
+
+namespace Mix.Shared.Services
+{
+    public class MixHeartConfigService : AppSettingServiceBase<MixHeartConfigurationModel>
+    {
+        public MixHeartConfigService(IConfiguration configuration)
+            : base(configuration, MixAppSettingsSection.MixHeart, MixAppConfigFilePaths.MixHeart)
+        {
+        }
+
+        public MixCacheDbProvider DatabaseProvider
+        {
+            get => AppSettings.DatabaseProvider;
+        }
+
+        public string GetConnectionString(string name)
+        {
+            switch (name)
+            {
+                case MixHeartConstants.CACHE_CONNECTION:
+                    return AppSettings.ConnectionStrings.CacheConnection;
+                case MixHeartConstants.AUDIT_LOG_CONNECTION:
+                    return AppSettings.ConnectionStrings.AuditLogConnection;
+                default:
+                    return string.Empty;
+            }
+        }
+
+        public void SetConnectionString(string name, string value)
+        {
+            switch (name)
+            {
+                case MixHeartConstants.CACHE_CONNECTION:
+                    AppSettings.ConnectionStrings.CacheConnection = value;
+                    break;
+                case MixHeartConstants.AUDIT_LOG_CONNECTION:
+                    AppSettings.ConnectionStrings.AuditLogConnection = value;
+                    break;
+                default:
+                    break;
+            }
+            SaveSettings();
+        }
+
+        protected override void BindAppSettings(IConfigurationSection settings)
+        {
+            try
+            {
+                AppSettings = new();
+                settings.Bind(AppSettings);
+            }
+            catch (Exception ex)
+            {
+                throw new MixException($"Cannot load config section {_sectionName}: {ex.Message}");
+            }
+        }
+    }
+}
