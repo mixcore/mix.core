771ab1017a4917b03d70e6c9fdfe96750d17845c
Hoang Nguyen Nhat <hoang.nguyen@orientsoftware.net>
2021-12-04 13:51:35 +0700

    Add  Mix FIle Controller

---
+++ src/modules/mix.portal/Domain/Models/MixFileResponseModel.cs
@@ -0,0 +1,11 @@
+﻿using Mix.Heart.Models;
+
+namespace Mix.Portal.Domain.Models
+{
+    public class MixFileResponseModel
+    {
+        public List<FileModel> Files { get; set; }
+
+        public List<string> Directories { get; set; }
+    }
+}
