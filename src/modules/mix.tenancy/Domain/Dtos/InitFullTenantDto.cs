443e127f8cf06ad1072df2eabbac2216be7ace84
phong.cao <phong.cao@orientsoftware.com>
2022-02-11 22:15:09 +0700

    feat: add init full tenant api

---
+++ src/modules/mix.tenancy/Domain/Dtos/InitFullTenantDto.cs
@@ -0,0 +1,13 @@
+﻿using Mix.Identity.Models.AccountViewModels;
+
+namespace Mix.Tenancy.Domain.Dtos
+{
+    public class InitFullSiteDto
+    {
+        public InitCmsDto TenantData { get; set; }
+
+        public RegisterViewModel AccountData { get; set; }
+
+        public InitFullSiteDto() { }
+    }
+}
