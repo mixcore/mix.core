7815ea903ab086d0daf5ea1db0a4f95b1c9c1aea
Hoang Nguyen Nhat <hoang.nguyen@orientsoftware.net>
2022-03-05 12:34:47 +0700

    update mixdata not require culture

--- src/platform/mix.database/EntityConfigurations/MYSQL/MixDataContentConfiguration.cs
+++ src/platform/mix.database/EntityConfigurations/MYSQL/MixDataContentConfiguration.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Mix.Database.Entities.Cms;
 using Mix.Database.EntityConfigurations.MYSQL.Base;
 using System;
@@ -10,6 +11,10 @@ namespace Mix.Database.EntityConfigurations.MYSQL
         public override void Configure(EntityTypeBuilder<MixDataContent> builder)
         {
             base.Configure(builder);
+            builder.Property(e => e.Specificulture)
+                .HasColumnType($"{Config.NString}{Config.SmallLength}")
+                .HasCharSet(Config.CharSet)
+                .UseCollation(Config.DatabaseCollation);
         }
     }
 }
