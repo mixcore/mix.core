9c5acc0ebad54482b7017cb4d8283bd44bf4df7e
Hoang Nguyen Nhat <hoang.nguyen@orientsoftware.net>
2022-03-06 16:24:56 +0700

    fix gen new encrypt key on init

--- src/modules/mix.tenancy/Domain/Services/InitCmsService.Site.cs
+++ src/modules/mix.tenancy/Domain/Services/InitCmsService.Site.cs
@@ -18,6 +18,7 @@ namespace Mix.Tenancy.Domain.Services
             InitTenantViewModel vm = new(_context);
             vm.InitSiteData(model);
             await vm.SaveAsync();
+            GlobalConfigService.Instance.AppSettings.ApiEncryptKey = AesEncryptionHelper.GenerateCombinedKeys(128);
             GlobalConfigService.Instance.AppSettings.DefaultCulture = model.Culture.Specificulture;
             GlobalConfigService.Instance.AppSettings.InitStatus = InitStep.InitTenant;
             GlobalConfigService.Instance.SaveSettings();
