32a79a703b1078eec130052517e16d30cbf6996a
Hoang Nguyen Nhat <hoang.nguyen@orientsoftware.net>
2022-04-16 12:47:58 +0700

    cleanup default theme

---
+++ src/modules/mix.portal/Domain/ViewModels/MixPageViewModel.cs
@@ -0,0 +1,31 @@
+﻿namespace Mix.Portal.Domain.ViewModels
+{
+    [GenerateRestApiController]
+    public class MixPageViewModel
+        : SiteDataWithContentViewModelBase
+        <MixCmsContext, MixPage, int, MixPageViewModel, MixPageContent, MixPageContentViewModel>
+    {
+        #region Contructors
+
+        public MixPageViewModel()
+        {
+        }
+
+        public MixPageViewModel(MixPage entity,
+
+            UnitOfWorkInfo uowInfo = null)
+            : base(entity, uowInfo)
+        {
+        }
+
+        public MixPageViewModel(UnitOfWorkInfo unitOfWorkInfo) : base(unitOfWorkInfo)
+        {
+        }
+
+        #endregion
+
+        #region Overrides
+
+        #endregion
+    }
+}
