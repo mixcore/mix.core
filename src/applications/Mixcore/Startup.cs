972e9821b4d495bcc8151372f11b5f1dc17d66b6
Hoang Nguyen Nhat <hoang.nguyen@orientsoftware.net>
2022-04-22 20:00:48 +0700

    update ocelot

--- src/applications/Mixcore/Startup.cs
+++ src/applications/Mixcore/Startup.cs
@@ -42,7 +42,7 @@ namespace Mixcore
             services.AddMixRoutes();
 
             // Must app Auth config after Add mixservice to init App config 
-            services.AddOcelot(Configuration);
+            services.AddMixOcelot(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
