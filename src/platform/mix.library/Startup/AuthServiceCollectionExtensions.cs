9a858562a436faae703043ebb1f1c87643a9007a
Hoang Nguyen Nhat <hoang.nguyen@orientsoftware.net>
2022-05-28 11:29:31 +0700

    update signalR authorize

--- src/platform/mix.library/Startup/AuthServiceCollectionExtensions.cs
+++ src/platform/mix.library/Startup/AuthServiceCollectionExtensions.cs
@@ -88,6 +88,29 @@ namespace Microsoft.Extensions.DependencyInjection
                                 ValidAudiences = authConfigurations.Audiences.Split(','),
                                 IssuerSigningKey = JwtSecurityKey.Create(authConfigurations.SecretKey)
                             };
+                    // Sending the access token in the query string is required due to
+                    // a limitation in Browser APIs. We restrict it to only calls to the
+                    // SignalR hub in this code.
+                    // See https://docs.microsoft.com/aspnet/core/signalr/security#access-token-logging
+                    // for more information about security considerations when using
+                    // the query string to transmit the access token.
+                    options.Events = new JwtBearerEvents
+                    {
+                        OnMessageReceived = context =>
+                        {
+                            var accessToken = context.Request.Query["access_token"];
+
+                            // If the request is for our hub...
+                            var path = context.HttpContext.Request.Path;
+                            if (!string.IsNullOrEmpty(accessToken) &&
+                                (path.StartsWithSegments("/hub")))
+                            {
+                                // Read the token out of the query string
+                                context.Token = accessToken;
+                            }
+                            return Task.CompletedTask;
+                        }
+                    };
                 });
 
             services.ConfigureApplicationCookie(options =>
--- src/platform/mix.signalr/Hubs/PortalHub.cs
+++ src/platform/mix.signalr/Hubs/PortalHub.cs
@@ -1,10 +1,22 @@
-﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
-
-using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authorization;
+using Mix.SignalR.Models;
+using System.Threading.Tasks;
 
 namespace Mix.SignalR.Hubs
 {
+    [Authorize]
     public class PortalHub : BaseSignalrHub
     {
+        public override async Task JoinRoom(string room)
+        {
+            
+            var msg = new SignalRMessageModel<object>()
+            {
+                Title = $"{Context.User.Identity.Name} joined {room}",
+                Message = $"New member {Context.ConnectionId}"
+            };
+            await Groups.AddToGroupAsync(Context.ConnectionId, room);
+            await SendMessageToGroups(msg.ToString(), room);
+        }
     }
 }
