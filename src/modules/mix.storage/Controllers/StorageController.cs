43860d7ca4792d4440b4c1283d3feca3614eef18
Hoang Nguyen Nhat <nhathoang989@gmail.com>
2022-06-10 17:38:15 +0700

    update mix storage

--- src/modules/mix.storage/Controllers/StorageController.cs
+++ src/modules/mix.storage/Controllers/StorageController.cs
@@ -1,15 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Mix.Shared.Services;
+using Mix.Storage.Lib.ViewModels;
 
 namespace Mix.Storage.Controllers
 {
-    [Route("api/v2/rest/mix-storage/media")]
+    [Route("api/v2/rest/mix-storage")]
     [ApiController]
-    public class StorageController : ControllerBase
+    public class StorageController : MixRestApiControllerBase<MixMediaViewModel, MixCmsContext, MixMedia, Guid>
     {
         private MixStorageService _storageService;
 
-        public StorageController(MixStorageService storageService)
+        public StorageController(MixStorageService storageService, 
+            IHttpContextAccessor httpContextAccessor, 
+            IConfiguration configuration, 
+            MixService mixService, 
+            TranslatorService translator, 
+            EntityRepository<MixCmsContext, MixCulture, int> cultureRepository, 
+            MixIdentityService mixIdentityService, 
+            UnitOfWorkInfo<MixCacheDbContext> cacheUOW, 
+            UnitOfWorkInfo<MixCmsContext> uow, 
+            IQueueService<MessageQueueModel> queueService) : base(httpContextAccessor, configuration, mixService, translator, cultureRepository, mixIdentityService, cacheUOW, uow, queueService)
         {
             _storageService = storageService;
         }
@@ -18,12 +28,12 @@ namespace Mix.Storage.Controllers
 
         [Route("upload-file")]
         [HttpPost]
-        public async Task<ActionResult> Upload([FromForm] string folder, [FromForm] IFormFile file)
+        public async Task<ActionResult> Upload([FromForm] string? folder, [FromForm] IFormFile file)
         {
             if (ModelState.IsValid)
             {
-                await _storageService.UploadFile(folder, file);
-                return Ok();
+                var result = await _storageService.UploadFile(folder, file, MixTenantId, User?.Identity?.Name);
+                return Ok(result);
             }
             return BadRequest();
         }
