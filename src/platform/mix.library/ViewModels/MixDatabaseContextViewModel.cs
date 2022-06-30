d238db1a4c83b2855590e0ad3d08ad429b9f8d6f
Hoang Nguyen Nhat <nhathoang989@gmail.com>
2022-06-30 14:43:03 +0700

    update mix db

---
+++ src/platform/mix.library/ViewModels/MixDatabaseContextViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mix.Lib.ViewModels
+{
+    [GenerateRestApiController]
+    public class MixDatabaseContextViewModel
+        : TenantDataViewModelBase<MixCmsContext, MixDatabaseContext, int, MixDatabaseContextViewModel>
+    {
+        #region Properties
+        public MixDatabaseProvider DatabaseProvider { get; set; }
+        public string ConnectionString { get; set; }
+
+        public List<MixDatabaseViewModel> Databases { get; set; } = new();
+        #endregion
+
+        #region Constructors
+
+        public MixDatabaseContextViewModel()
+        {
+
+        }
+
+        public MixDatabaseContextViewModel(UnitOfWorkInfo unitOfWorkInfo) : base(unitOfWorkInfo)
+        {
+        }
+
+        public MixDatabaseContextViewModel(MixDatabaseContext entity, UnitOfWorkInfo uowInfo = null)
+            : base(entity, uowInfo)
+        {
+        }
+
+        #endregion
+
+        #region Overrides
+        public override async Task ExpandView()
+        {
+            var dbRepo = MixDatabaseViewModel.GetRepository(UowInfo);
+            var associations = Context.MixDatabaseContextDatabaseAssociation.Where(m => m.LeftId == Id).Select(m => m.RightId);
+            Databases = await dbRepo.GetListAsync(m => associations.Any(a => a == m.Id));
+        }
+
+        protected override async Task DeleteHandlerAsync()
+        {
+            var associations = Context.MixDatabaseContextDatabaseAssociation.Where(m => m.LeftId == Id);
+            Context.MixDatabaseContextDatabaseAssociation.RemoveRange(associations);
+            await Context.SaveChangesAsync();
+            await base.DeleteHandlerAsync();
+        }
+
+        #endregion
+    }
+}
