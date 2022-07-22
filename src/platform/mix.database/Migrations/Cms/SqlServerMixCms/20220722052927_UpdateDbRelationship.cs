4e81858c3ef5927ee3b84457cbc582c8b1f7f0ad
Hoang Nguyen Nhat <nhathoang989@gmail.com>
2022-07-22 12:30:00 +0700

    Update database migration - relationship

---
+++ src/platform/mix.database/Migrations/Cms/SqlServerMixCms/20220722052927_UpdateDbRelationship.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mix.Database.Migrations.SqlServerMixCms
+{
+    public partial class UpdateDbRelationship : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DestinateDatabaseName",
+                table: "MixDatabaseRelationship",
+                type: "varchar(50)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "SourceDatabaseName",
+                table: "MixDatabaseRelationship",
+                type: "varchar(50)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DestinateDatabaseName",
+                table: "MixDatabaseRelationship");
+
+            migrationBuilder.DropColumn(
+                name: "SourceDatabaseName",
+                table: "MixDatabaseRelationship");
+        }
+    }
+}
