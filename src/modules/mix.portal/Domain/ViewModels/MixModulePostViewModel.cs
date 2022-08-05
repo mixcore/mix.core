fa208f55e6532438c62b2c8934410f19baf547b2
Eric Nguyen <nhathoang989@gmail.com>
2022-08-05 18:57:46 +0700

    add module-post apis

---
+++ src/modules/mix.portal/Domain/ViewModels/MixModulePostViewModel.cs
@@ -0,0 +1,35 @@
+﻿namespace Mix.Portal.Domain.ViewModels
+{
+    public class MixModulePostAssociationViewModel
+        : AssociationViewModelBase<MixCmsContext, MixModulePostAssociation, int, MixModulePostAssociationViewModel>
+    {
+        #region Properties
+        #endregion
+
+        #region Constructors
+
+        public MixModulePostAssociationViewModel()
+        {
+        }
+
+        public MixModulePostAssociationViewModel(MixModulePostAssociation entity,
+
+            UnitOfWorkInfo uowInfo = null)
+            : base(entity, uowInfo)
+        {
+        }
+
+        public MixModulePostAssociationViewModel(UnitOfWorkInfo unitOfWorkInfo) : base(unitOfWorkInfo)
+        {
+        }
+
+        #endregion
+
+        #region Overrides
+        #endregion
+
+        #region Expands
+
+        #endregion
+    }
+}
