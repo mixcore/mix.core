8435095cefd824bff934ae511b6eec23b012cc14
Eric Nguyen <nhathoang989@gmail.com>
2022-08-05 18:57:46 +0700

    add module-post apis

---
+++ src/modules/mix.portal/Controllers/MixModulePostPortalController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Mix.Portal.Controllers
+{
+    [Route("api/v2/rest/mix-portal/mix-module-post")]
+    [ApiController]
+    public class MixModulePostController
+        : MixAssociationApiControllerBase<MixModulePostAssociationViewModel, MixCmsContext, MixModulePostAssociation>
+    {
+        private readonly UnitOfWorkInfo<MixCmsContext> _cmsUOW;
+
+        public MixModulePostController(
+            IHttpContextAccessor httpContextAccessor,
+            IConfiguration configuration,
+            MixService mixService,
+            TranslatorService translator,
+            EntityRepository<MixCmsContext, MixCulture, int> cultureRepository,
+            MixIdentityService mixIdentityService,
+            UnitOfWorkInfo<MixCacheDbContext> cacheUOW,
+            UnitOfWorkInfo<MixCmsContext> cmsUOW,
+            IQueueService<MessageQueueModel> queueService)
+            : base(httpContextAccessor, configuration, mixService, translator, cultureRepository, mixIdentityService, cacheUOW, cmsUOW, queueService)
+        {
+            _cmsUOW = cmsUOW;
+        }
+
+        #region Overrides
+
+        protected override Task<int> CreateHandlerAsync(MixModulePostAssociationViewModel data)
+        {
+            if (_cmsUOW.DbContext.MixModulePostAssociation.Any(
+                m => m.MixTenantId == MixTenantId
+                && m.ParentId == data.ParentId
+                && m.ChildId == data.ChildId))
+            {
+                throw new MixException(MixErrorStatus.Badrequest, "Entity existed");
+            }
+            return base.CreateHandlerAsync(data);
+        }
+        #endregion
+
+
+    }
+}
