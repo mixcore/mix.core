f4a18b522596c9caaabd67a9b9d00e8537e61cf0
Eric Nguyen <nhathoang989@gmail.com>
2022-08-06 13:46:53 +0700

    update get Mix Db Association

---
+++ src/modules/mix.portal/Controllers/MixDatabaseAssociationController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Mix.Portal.Controllers
+{
+    [Route("api/v2/rest/mix-portal/mix-database-association")]
+    [ApiController]
+    [MixAuthorize($"{MixRoles.SuperAdmin}, {MixRoles.Owner}")]
+    public class MixDatabaseAssociationController
+        : MixRestApiControllerBase<MixDatabaseAssociationViewModel, MixCmsContext, MixDatabaseAssociation, Guid>
+    {
+        public MixDatabaseAssociationController(
+            IHttpContextAccessor httpContextAccessor,
+            IConfiguration configuration,
+            MixService mixService,
+            TranslatorService translator,
+            EntityRepository<MixCmsContext, MixCulture, int> cultureRepository,
+            MixIdentityService mixIdentityService,
+            UnitOfWorkInfo<MixCacheDbContext> cacheUOW,
+            UnitOfWorkInfo<MixCmsContext> cmsUOW,
+            IQueueService<MessageQueueModel> queueService)
+            : base(httpContextAccessor, configuration, mixService, translator, cultureRepository, mixIdentityService, cacheUOW, cmsUOW, queueService)
+        {
+
+        }
+
+        #region Overrides
+
+
+        #endregion
+
+
+    }
+}
---
+++ src/platform/mix.library/ViewModels/MixDatabaseAssociationViewModel.cs
@@ -0,0 +1,50 @@
+﻿using Mix.Lib.Services;
+
+namespace Mix.Lib.ViewModels
+{
+    public class MixDatabaseAssociationViewModel
+        : ViewModelBase<MixCmsContext, MixDatabaseAssociation, Guid, MixDatabaseAssociationViewModel>
+    {
+        #region Properties
+        public int MixTenantId { get; set; }
+        public string ParentDatabaseName { get; set; }
+        public string ChildDatabaseName { get; set; }
+        public int ParentId { get; set; }
+        public int ChildId { get; set; }
+        #endregion
+
+        #region Constructors
+
+        public MixDatabaseAssociationViewModel()
+        {
+        }
+
+        public MixDatabaseAssociationViewModel(MixDatabaseAssociation entity,
+
+            UnitOfWorkInfo uowInfo = null)
+            : base(entity, uowInfo)
+        {
+        }
+
+        public MixDatabaseAssociationViewModel(UnitOfWorkInfo unitOfWorkInfo) : base(unitOfWorkInfo)
+        {
+        }
+
+        #endregion
+
+        #region Overrides
+        public override void InitDefaultValues(string language = null, int? cultureId = null)
+        {
+            base.InitDefaultValues(language, cultureId);
+            if(Id == default)
+            {
+                Id = Guid.NewGuid();
+            }
+        }
+        #endregion
+
+        #region Expands
+
+        #endregion
+    }
+}
