be1225005e582eae70a11c1c438fe8da5b11c5df
Eric Nguyen <nhathoang989@gmail.com>
2022-08-11 15:59:29 +0700

    Init Auto generate Mix database DbContext

---
+++ src/platform/mix.database/Entities/Runtime/RuntimeDbContext.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Loader;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.EntityFrameworkCore.Scaffolding;
+using Microsoft.EntityFrameworkCore.Scaffolding.Internal;
+using Microsoft.EntityFrameworkCore.SqlServer.Diagnostics.Internal;
+using Microsoft.EntityFrameworkCore.SqlServer.Scaffolding.Internal;
+using Microsoft.EntityFrameworkCore.SqlServer.Storage.Internal;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.EntityFrameworkCore.Sqlite.Diagnostics.Internal;
+using Microsoft.EntityFrameworkCore.Sqlite.Storage.Internal;
+using Microsoft.EntityFrameworkCore.Sqlite.Scaffolding.Internal;
+
+namespace Mix.Database.Entities.Runtime
+{
+    public class RuntimeDbContext
+    {
+        public static List<string> CreateDynamicDbContext(MixDatabaseProvider databaseProvider, string connectionString)
+        {
+            var scaffolder = CreateScaffolder();
+
+            var dbOpts = new DatabaseModelFactoryOptions();
+            var modelOpts = new ModelReverseEngineerOptions();
+            var codeGenOpts = new ModelCodeGenerationOptions()
+            {
+                RootNamespace = "TypedDataContext",
+                ContextName = "DataContext",
+                ContextNamespace = "TypedDataContext.Context",
+                ModelNamespace = "TypedDataContext.Models",
+                SuppressConnectionStringWarning = true
+            };
+
+            var scaffoldedModelSources = scaffolder.ScaffoldModel(connectionString, dbOpts, modelOpts, codeGenOpts);
+            var sourceFiles = new List<string> { scaffoldedModelSources.ContextFile.Code };
+            sourceFiles.AddRange(scaffoldedModelSources.AdditionalFiles?.Select(f => f.Code));
+
+            return sourceFiles;
+        }
+
+        [SuppressMessage("Usage", "EF1001:Internal EF Core API usage.", Justification = "We need it")]
+        static IReverseEngineerScaffolder CreateScaffolder() =>
+           new ServiceCollection()
+                .AddEntityFrameworkSqlite()
+                .AddLogging()
+                .AddEntityFrameworkDesignTimeServices()
+                .AddSingleton<ProviderCodeGeneratorDependencies>()
+                .AddSingleton<AnnotationCodeGeneratorDependencies>()
+                .AddSingleton<LoggingDefinitions, SqliteLoggingDefinitions>()
+                .AddSingleton<IRelationalTypeMappingSource, SqliteTypeMappingSource>()
+                .AddSingleton<IAnnotationCodeGenerator, AnnotationCodeGenerator>()
+                .AddSingleton<IDatabaseModelFactory, SqliteDatabaseModelFactory>()
+                .AddSingleton<IProviderConfigurationCodeGenerator, SqliteCodeGenerator>()
+                .AddSingleton<IScaffoldingModelFactory, RelationalScaffoldingModelFactory>()
+                .AddSingleton<IReverseEngineerScaffolder, ReverseEngineerScaffolder>()
+                .BuildServiceProvider()
+                .GetRequiredService<IReverseEngineerScaffolder>();
+
+
+        static List<MetadataReference> CompilationReferences(bool enableLazyLoading)
+        {
+            var refs = new List<MetadataReference>();
+            var referencedAssemblies = Assembly.GetExecutingAssembly().GetReferencedAssemblies();
+            refs.AddRange(referencedAssemblies.Select(a => MetadataReference.CreateFromFile(Assembly.Load(a).Location)));
+
+            refs.Add(MetadataReference.CreateFromFile(typeof(object).Assembly.Location));
+            refs.Add(MetadataReference.CreateFromFile(Assembly.Load("netstandard, Version=2.0.0.0").Location));
+            refs.Add(MetadataReference.CreateFromFile(typeof(System.Data.Common.DbConnection).Assembly.Location));
+            refs.Add(MetadataReference.CreateFromFile(typeof(System.Linq.Expressions.Expression).Assembly.Location));
+
+            if (enableLazyLoading)
+            {
+                refs.Add(MetadataReference.CreateFromFile(typeof(ProxiesExtensions).Assembly.Location));
+            }
+
+            return refs;
+        }
+
+        public static CSharpCompilation GenerateCode(List<string> sourceFiles, bool enableLazyLoading)
+        {
+            var options = CSharpParseOptions.Default.WithLanguageVersion(LanguageVersion.CSharp8);
+
+            var parsedSyntaxTrees = sourceFiles.Select(f => SyntaxFactory.ParseSyntaxTree(f, options));
+
+            return CSharpCompilation.Create($"DataContext.dll",
+                parsedSyntaxTrees,
+                references: CompilationReferences(enableLazyLoading),
+                options: new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary,
+                    optimizationLevel: OptimizationLevel.Release,
+                    assemblyIdentityComparer: DesktopAssemblyIdentityComparer.Default));
+        }
+
+
+        //public static class DynamicContextExtensions
+        //{
+        //    public static IQueryable Query(this DbContext context, string entityName) =>
+        //        context.Query(entityName, context.Model.FindEntityType(entityName).ClrType);
+
+        //    static readonly MethodInfo SetMethod =
+        //        typeof(DbContext).GetMethod(nameof(DbContext.Set), 1, new[] { typeof(string) }) ??
+        //        throw new Exception($"Type not found: DbContext.Set");
+
+        //    public static IQueryable Query(this DbContext context, string entityName, Type entityType) =>
+        //        (IQueryable)SetMethod.MakeGenericMethod(entityType)?.Invoke(context, new[] { entityName }) ??
+        //        throw new Exception($"Type not found: {entityType.FullName}");
+        //}
+    }
+}
