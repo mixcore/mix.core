3f76aa9d9ea0099f92e6a2833084cd686e3685b2
Eric Nguyen <nhathoang989@gmail.com>
2022-09-08 14:59:15 +0700

    fix portal route

--- src/platform/mix.library/Startup/_ServiceCollectionExtensions.cs
+++ src/platform/mix.library/Startup/_ServiceCollectionExtensions.cs
@@ -97,6 +97,7 @@ namespace Microsoft.Extensions.DependencyInjection
             services.TryAddSingleton<AuditLogService>();
             services.TryAddSingleton<MixMemoryCacheService>();
             services.AddSingleton<PortalHubClientService>();
+            services.AddMixRepoDb();
             return services;
         }
 
