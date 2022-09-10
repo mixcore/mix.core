b815f16f187ddbb8d071771c7775c796bdb1121d
Eric Nguyen <nhathoang989@gmail.com>
2022-09-10 11:32:48 +0700

    fix bugs

---
+++ src/modules/mix.common/Controllers/PostContentApiController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Mix.Heart.Entities.Cache;
+using Mix.Heart.Models;
+using Mix.Lib.Services;
+using Mix.Queue.Interfaces;
+using Mix.Queue.Models;
+using Mix.RepoDb.Repositories;
+using Mixcore.Domain.ViewModels;
+
+namespace Mix.Common.Controllers
+{
+    [Route("api/v2/rest/post-content")]
+    [ApiController]
+    public class PostContentApiController : MixQueryApiControllerBase<PostContentViewModel, MixCmsContext, MixPostContent, int>
+    {
+        private readonly MixRepoDbRepository _mixRepoDbRepository;
+        public PostContentApiController(
+            IHttpContextAccessor httpContextAccessor,
+            IConfiguration configuration,
+            MixService mixService,
+            TranslatorService translator,
+            EntityRepository<MixCmsContext, MixCulture, int> cultureRepository,
+            MixIdentityService mixIdentityService,
+            UnitOfWorkInfo<MixCacheDbContext> cacheUOW,
+            UnitOfWorkInfo<MixCmsContext> uow,
+            IQueueService<MessageQueueModel> queueService,
+            MixRepoDbRepository mixRepoDbRepository)
+            : base(httpContextAccessor, configuration, mixService, translator, cultureRepository, mixIdentityService, cacheUOW, uow, queueService)
+        {
+            _mixRepoDbRepository = mixRepoDbRepository;
+        }
+
+        protected override async Task<PostContentViewModel> GetById(int id)
+        {
+            var result = await base.GetById(id);
+            await result.LoadAdditionalDataAsync(_mixRepoDbRepository);
+            return result;
+        }
+
+        protected override async Task<PagingResponseModel<PostContentViewModel>> SearchHandler(SearchRequestDto req)
+        {
+            var result = await base.SearchHandler(req);
+            List<Task> tasks = new();
+            foreach (var post in result.Items)
+            {
+                tasks.Add(post.LoadAdditionalDataAsync(_mixRepoDbRepository));
+            }
+            await Task.WhenAll(tasks);
+            return result;
+        }
+    }
+}
