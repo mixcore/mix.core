cc67d05bbce628d8a2538300f59b9372bf2ea540
Nhật Hoàng <nhathoang989@gmail.com>
2022-09-25 14:00:48 +0700

    Init Universal Service

---
+++ src/services/mix.universal.lib/Entities/MixUniversalDbContext.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Mix.Constant.Constants;
+using Mix.Database.Entities.Account;
+using Mix.Database.EntityConfigurations.MYSQL;
+using Mix.Database.EntityConfigurations.POSTGRES;
+using Mix.Database.EntityConfigurations.SQLITE;
+using Mix.Database.EntityConfigurations.SQLSERVER;
+using Mix.Database.Services;
+using Mix.Heart.EntityFrameworkCore.Extensions;
+using Mix.Heart.Enums;
+using Mix.Universal.Lib.EntityConfigurations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mix.Universal.Lib.Entities
+{
+    public class MixUniversalDbContext: DbContext
+    {
+        public virtual DbSet<Organization> Organizations { get; set; }
+
+        private static DatabaseService _databaseService;
+        public MixUniversalDbContext(
+            DatabaseService databaseService)
+                    : base()
+        {
+            _databaseService = databaseService;
+        }
+        protected override void OnConfiguring(
+           DbContextOptionsBuilder optionsBuilder)
+        {
+            string cnn = _databaseService.GetConnectionString(MixConstants.CONST_CMS_CONNECTION);
+            if (!string.IsNullOrEmpty(cnn))
+            {
+                switch (_databaseService.DatabaseProvider)
+                {
+                    case MixDatabaseProvider.SQLSERVER:
+                        optionsBuilder.UseSqlServer(cnn);
+                        break;
+
+                    case MixDatabaseProvider.MySQL:
+                        optionsBuilder.UseMySql(cnn, ServerVersion.AutoDetect(cnn));
+                        break;
+
+                    case MixDatabaseProvider.SQLITE:
+                        optionsBuilder.UseSqlite(cnn);
+                        break;
+
+                    case MixDatabaseProvider.PostgreSQL:
+                        optionsBuilder.UseNpgsql(cnn);
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(this.GetType().Assembly);
+        }
+    }
+}
---
+++ src/services/mix.universal.lib/Helpers/MixUniversalHelper.cs
@@ -0,0 +1,18 @@
+﻿using Mix.Heart.Helpers;
+using Mix.Identity.Models.AccountViewModels;
+using Mix.Universal.Lib.Dtos;
+using Newtonsoft.Json.Linq;
+
+namespace Mix.Universal.Lib.Helpers
+{
+    public class MixUniversalHelper
+    {
+        public static RegisterViewModel ParseRegisterDto(RegisterDto dto)
+        {
+            RegisterViewModel result = new();
+            ReflectionHelper.Mapping(dto, result);
+            result.Data = JObject.FromObject(dto.Information);
+            return result;
+        }
+    }
+}
