b9a490c980777534ad14055a7be176a78ed5aa04
Nhật Hoàng <nhathoang989@gmail.com>
2022-09-26 14:40:41 +0700

    update

---
+++ src/services/mix.universal.lib/ViewModels/PortalAppViewModel.cs
@@ -0,0 +1,47 @@
+﻿using Mix.Heart.UnitOfWork;
+using Mix.Heart.ViewModel;
+using Mix.Lib.Attributes;
+using Mix.Universal.Lib.Entities;
+using Org.BouncyCastle.Asn1.Crmf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mix.Universal.Lib.ViewModels
+{
+    [GenerateRestApiController]
+    public class PortalAppViewModel : ViewModelBase<MixUniversalDbContext, PortalApp, int, PortalAppViewModel>
+    {
+        #region Properties
+
+        public string Title { get; set; }
+        public string Path { get; set; }
+        public string MixcoreVersion { get; set; }
+        public int OrganizationId { get; set; }
+        public int? TenantId { get; set; }
+
+        #endregion
+
+        #region Contructors
+
+        public PortalAppViewModel()
+        {
+        }
+
+        public PortalAppViewModel(MixUniversalDbContext context) : base(context)
+        {
+        }
+
+        public PortalAppViewModel(UnitOfWorkInfo unitOfWorkInfo) : base(unitOfWorkInfo)
+        {
+        }
+
+        public PortalAppViewModel(PortalApp entity, UnitOfWorkInfo? uowInfo = null) : base(entity, uowInfo)
+        {
+        }
+
+        #endregion
+    }
+}
--- src/services/mix.universal/Domain/StartupService.cs
+++ src/services/mix.universal/Domain/StartupService.cs
@@ -1,5 +1,8 @@
-﻿using Mix.Shared.Interfaces;
+﻿using Microsoft.Extensions.DependencyInjection.Extensions;
+using Mix.Lib.Middlewares;
+using Mix.Shared.Interfaces;
 using Mix.Universal.Lib.Entities;
+using Mix.Universal.Lib.Middlewares;
 
 namespace Mix.Universal.Domain
 {
@@ -8,10 +11,12 @@ namespace Mix.Universal.Domain
         public void AddServices(IServiceCollection services, IConfiguration configuration)
         {
             services.AddDbContext<MixUniversalDbContext>();
+            services.TryAddScoped<UnitOfWorkInfo<MixUniversalDbContext>>();
         }
 
         public void UseApps(IApplicationBuilder app, IConfiguration configuration, bool isDevelop)
         {
+            app.UseMiddleware<MixUniversalUnitOfWorkMiddleware>();
         }
     }
 }
