231af789e25059ec14145975bbe0e43bf0a2b813
Eric Nguyen <nhathoang989@gmail.com>
2022-10-06 12:29:51 +0700

    init mix application

---
+++ src/platform/mix.database/Entities/Cms/MixApplication.cs
@@ -0,0 +1,20 @@
+﻿using Mix.Database.Entities.Base;
+using System.Collections.Generic;
+
+namespace Mix.Database.Entities.Cms
+{
+    public class MixApplication : TenantEntityBase<int>
+    {
+        public string Title { get; set; }
+        public string BaseHref { get; set; }
+        public string BaseRoute { get; set; }
+        public string Domain { get; set; }
+        public string BaseApiUrl { get; set; }
+        public int? TemplateId { get; set; }
+        public string Image { get; set; }
+        public string MixDatabaseName { get; set; }
+        public Guid? MixDataContentId { get; set; }
+
+        public MixDataContent MixDataContent { get; set; }
+    }
+}
