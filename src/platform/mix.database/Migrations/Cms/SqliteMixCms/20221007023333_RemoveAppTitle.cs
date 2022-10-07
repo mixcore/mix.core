0eddd66159856b26def6b4618635f961d4ad68ab
Hoang Nguyen Nhat <nhathoang989@gmail.com>
2022-10-07 09:43:10 +0700

    update install app

---
+++ src/platform/mix.database/Migrations/Cms/SqliteMixCms/20221007023333_RemoveAppTitle.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mix.Database.Migrations.SqliteMixCms
+{
+    public partial class RemoveAppTitle : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "MixApplication");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "MixApplication",
+                type: "TEXT",
+                nullable: true);
+        }
+    }
+}
