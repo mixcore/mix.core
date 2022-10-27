52d9ca5c72b9b94b6bb3a08d039e919906a7608d
Eric Nguyen <nhathoang989@gmail.com>
2022-10-27 10:48:21 +0700

    clean code

--- src/platform/mix.shared/Dtos/SearchRequestDto.cs
+++ src/platform/mix.shared/Dtos/SearchRequestDto.cs
@@ -4,18 +4,18 @@ namespace Mix.Shared.Dtos
 {
     public class SearchRequestDto
     {
-        public string? Keyword { get; set; }
-        public string? Culture { get; set; }
+        public string Keyword { get; set; } = default;
+        public string Culture { get; set; } = default;
         public DateTime? FromDate { get; set; }
         public DateTime? ToDate { get; set; }
         public int PageIndex { get; set; }
         public int? PageSize { get; set; }
-        public string? OrderBy { get; set; }
+        public string OrderBy { get; set; } = default;
         public SortDirection Direction { get; set; }
         public MixContentStatus? Status { get; set; }
         public ExpressionMethod? SearchMethod { get; set; } = ExpressionMethod.Equal;
-        public string? Columns { get; set; }
-        public string? SearchColumns { get; set; }
-        
+        public string Columns { get; set; } = default;
+        public string SearchColumns { get; set; } = default;
+
     }
 }
