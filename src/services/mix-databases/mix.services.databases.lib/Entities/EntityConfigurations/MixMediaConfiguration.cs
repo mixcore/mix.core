e53a256573c41675e63a2482627cb2429e8dec4f
Eric Nguyen <nhathoang989@gmail.com>
2023-01-01 12:59:44 +0700

    fix upload media targeturl

---
+++ src/services/mix-databases/mix.services.databases.lib/Entities/EntityConfigurations/MixMediaConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Mix.Database.EntityConfigurations.Base;
+using Mix.Database.Services;
+
+namespace Mix.Services.Databases.Lib.Entities.EntityConfigurations
+{
+    public class MixMediaConfiguration : EntityBaseConfiguration<MixMedia, int>
+    {
+        public MixMediaConfiguration(DatabaseService databaseService) : base(databaseService)
+        {
+        }
+        public override void Configure(EntityTypeBuilder<MixMedia> builder)
+        {
+            builder.ToTable(MixServicesDatabasesConstants.DatabaseNameMedia);
+            base.Configure(builder);
+        }
+    }
+}
--- src/services/mix-databases/mix.services.databases.lib/Entities/MixServiceDatabaseDbContext.cs
+++ src/services/mix-databases/mix.services.databases.lib/Entities/MixServiceDatabaseDbContext.cs
@@ -13,6 +13,7 @@ namespace Mix.Services.Databases.Lib.Entities
             _dbContextType = GetType();
         }
 
+        public DbSet<MixMedia> MixMedia { get; set; }
         public DbSet<MixMetadata> MixMetadata { get; set; }
         public DbSet<MixMetadataContentAssociation> MixMetadataContentAssociation { get; set; }
         public DbSet<MixPermission> Permission { get; set; }
