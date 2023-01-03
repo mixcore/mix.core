798ff4297569767707b9d8b656a9ac0d9ec1919a
phong cao <phongcao3091998@gmail.com>
2023-01-03 16:28:16 +0700

    ecommerce: update selected cart

--- src/services/ecommerces/mix.services.ecommerce.lib/Dtos/CartItemDto.cs
+++ src/services/ecommerces/mix.services.ecommerce.lib/Dtos/CartItemDto.cs
@@ -9,6 +9,7 @@
         public string ReferenceUrl { get; set; }
         public int PostId { get; set; }
         public int Quantity { get; set; }
+        public bool IsActive { get; set; } = true;
         //public string? Currency { get; set; }
         //public double Price { get; set; }
     }
