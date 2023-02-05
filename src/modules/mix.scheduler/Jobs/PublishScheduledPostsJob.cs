7f60b1e423ae9022c5566991754a41a3b8c997f1
phat.truong <phat.truong@orientsoftware.com>
2023-02-05 19:32:08 +0700

    Move job to schedule project

--- src/modules/mix.scheduler/Jobs/PublishScheduledPostsJob.cs
+++ src/modules/mix.scheduler/Jobs/PublishScheduledPostsJob.cs
@@ -1,9 +1,11 @@
-﻿using Mix.Queue.Interfaces;
+﻿using Mix.MixQuartz.Jobs;
+using Mix.Queue.Interfaces;
 using Mix.Queue.Models;
+using Quartz;
 using System;
 using System.Threading.Tasks;
 
-namespace Mix.MixQuartz.Jobs
+namespace Mix.Scheduler.Jobs
 {
     public class PublishScheduledPostsJob : MixJobBase
     {
