4ac0d2b8a53c79eadd1d5affcfea33aff862e632
Eric Nguyen <nhathoang989@gmail.com>
2023-04-18 12:32:39 +0700

    mix - update auditlog viewmodel

--- src/platform/mix.library/ViewModels/AuditLogViewModel.cs
+++ src/platform/mix.library/ViewModels/AuditLogViewModel.cs
@@ -7,11 +7,13 @@ namespace Mix.Lib.ViewModels
         : ViewModelBase<AuditLogDbContext, AuditLog, Guid, AuditLogViewModel>
     {
         #region Properties
+        public bool Success { get; set; }
         public string RequestIp { get; set; }
         public string Endpoint { get; set; }
         public string Method { get; set; }
+        public string QueryString { get; set; }
         public JObject Body { get; set; }
-        public bool Success { get; set; }
+        public JObject Response { get; set; }
         public JObject Exception { get; set; }
         #endregion
 
