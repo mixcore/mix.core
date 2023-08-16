f9facc7aacd0f9f309d9176ef0a241d8458fe469
Hoang Nguyen Nhat <nhathoang989@gmail.com>
2023-08-16 16:07:57 +0700

    init db context

---
+++ src/platform/mix.database/Entities/Queue/EntityConfigurations/MixQueueMessageConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mix.Database.Entities.Queue.EntityConfigurations
+{
+    internal class MixQueueMessageConfiguration : IEntityTypeConfiguration<MixQueueMessage>
+    {
+        public IDatabaseConstants Config;
+        public MixQueueMessageConfiguration()
+        {
+            Config = new SqliteDatabaseConstants();
+        }
+        public void Configure(EntityTypeBuilder<MixQueueMessage> builder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/platform/mix.database/Entities/Queue/MixQueueMessage.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mix.Database.Entities.Queue
+{
+    public sealed class MixQueueMessage: EntityBase<Guid>
+    {
+        public string From { get; set; }
+        public string Action { get; set; }
+        public string TopicId { get; set; }
+        public string StringData { get; set; }
+        public JObject ObjectData { get; set; }
+        public string DataTypeFullName { get; set; }
+        public string State { get; set; }
+        public int TenantId { get; set; }
+    }
+}
