d8f86ad0a2cef951c6d341536a665e6886735db5
Phat <tanphat1991996@gmail.com>
2023-08-18 11:41:32 +0700

    update bash script

--- deployment/deployment.sh
+++ deployment/deployment.sh
@@ -5,25 +5,21 @@ HOST=$1
 USER=$2
 PASSWORD=$3
 
+LOCALFOLDER=$4
 
-# $1 is the first argument to the script
-# We are using it as upload directory path
-# If it is '.', file is uploaded to current directory.
-SOURCE=$4
-
-DESTINATION=$5
+REMOTEFOLDER=$5
 
 FILES="./*"
 
 # Go to local current directory
-cd "$SOURCE"
+cd "$LOCALFOLDER"
 
 # FTP login and upload is explained in paragraph below
 ftp -inv $HOST <<EOF
 user $USER $PASSWORD
 
 # Go to local current directory
-cd "$DESTINATION"
+cd "$REMOTEFOLDER"
 mput $FILES
 mdel app_offline.htm
 bye
