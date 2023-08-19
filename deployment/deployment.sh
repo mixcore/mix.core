6a5a5bf393e3a0f3ba54f60e96f1c2d723b0576e
Phat Truong <tanphat1991996@gmail.com>
2023-08-19 20:16:37 +0700

    Update deployment.sh

--- deployment/deployment.sh
+++ deployment/deployment.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # The 3 variables below store server and login details
-HOST=$1
-USER=$2
-PASSWORD=$3
+# HOST=$1
+# USER=$2
+# PASSWORD=$3
 
 LOCALFOLDER=$4
 
@@ -16,7 +16,7 @@ cd "$LOCALFOLDER"
 echo $1 $3 $3
 # FTP login and upload is explained in paragraph below
 ftp -inv $HOST <<EOF
-user $USER $PASSWORD
+user $USERNAME $PASSWORD
 
 # Go to local current directory
 cd "$REMOTEFOLDER"
