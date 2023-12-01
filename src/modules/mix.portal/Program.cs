d86a1191235eb6cbaf55373e9551de7805e7f148
Eric Nguyen <nhathoang989@gmail.com>
2023-12-01 20:27:48 +0700

    update portal

--- src/modules/mix.portal/Program.cs
+++ src/modules/mix.portal/Program.cs
@@ -1,13 +1,32 @@
-namespace Mix.Portal
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+using Mix.Database.Entities.Account;
+using System.Configuration;
+using System.Reflection;
+using Mix.Log.Lib;
+using Microsoft.Azure.Amqp.Framing;
+using Mix.Lib.Middlewares;
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-          MixCmsHelper.CreateHostBuilder<Startup>(args);
-    }
-}
+var builder = MixCmsHelper.CreateWebApplicationBuilder(args);
+
+builder.AddServiceDefaults();
+
+//MixFileHelper.CopyFolder(MixFolders.MixCoreConfigurationFolder, MixFolders.MixContentFolder);
+
+
+builder.Services.AddMixServices(Assembly.GetExecutingAssembly(), builder.Configuration);
+builder.Services.AddMixCors();
+builder.Services.AddMixLog(builder.Configuration);
+// Must app Auth config after Add mixservice to init App config 
+builder.Services.AddMixAuthorize<MixCmsAccountContext>(builder.Configuration);
+
+var app = builder.Build();
+
+app.UseMixCors();
+app.UseMixTenant();
+app.UseMiddleware<AuditlogMiddleware>();
+app.UseRouting();
+app.UseMixAuth();
+app.UseMixCors();
+app.UseRouting();
+app.UseMixApps(Assembly.GetExecutingAssembly(), builder.Configuration, builder.Environment.ContentRootPath, builder.Environment.IsDevelopment());
+
+app.Run();
