013af7952770146dc8250ab7b07239a52c3fcc34
Nhật Hoàng <nhathoang.nguyen@pizza4ps.com>
2025-03-03 13:05:14 +0700

    update default config

--- src/applications/mixcore.host.aspire.ServiceDefaults/Extensions.cs
+++ src/applications/mixcore.host.aspire.ServiceDefaults/Extensions.cs
@@ -103,7 +103,8 @@ public static class Extensions
     public static WebApplication MapDefaultEndpoints(this WebApplication app)
     {
         // Uncomment the following line to enable the Prometheus endpoint (requires the OpenTelemetry.Exporter.Prometheus.AspNetCore package)
-        app.MapPrometheusScrapingEndpoint();
+        app.MapPrometheusScrapingEndpoint()
+            .DisableHttpMetrics();
             //.RequireAuthorization();
 
         // All health checks must pass for app to be considered ready to accept traffic after starting
