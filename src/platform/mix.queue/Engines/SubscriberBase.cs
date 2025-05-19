437d149098c3609de4df9d32c5168b7e2373fb9a
Nhật Hoàng <nhathoang989@gmail.com>
2025-05-19 12:16:03 +0700

    fix postgres auditlog

--- src/platform/mix.queue/Engines/SubscriberBase.cs
+++ src/platform/mix.queue/Engines/SubscriberBase.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
