91a816d7ef17f5e05ffc61db6c7bf9619218e1a9
Nhật Hoàng <nhathoang989@gmail.com>
2025-05-19 09:20:39 +0700

    init sample spa

---
+++ src/applications/mixcore.spa/mixcore.spa.client/src/app/app.component.ts
@@ -0,0 +1,38 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+interface WeatherForecast {
+  date: string;
+  temperatureC: number;
+  temperatureF: number;
+  summary: string;
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  standalone: false,
+  styleUrl: './app.component.css'
+})
+export class AppComponent implements OnInit {
+  public forecasts: WeatherForecast[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.getForecasts();
+  }
+
+  getForecasts() {
+    this.http.get<WeatherForecast[]>('/weatherforecast').subscribe(
+      (result) => {
+        this.forecasts = result;
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  }
+
+  title = 'mixcore.spa.client';
+}
