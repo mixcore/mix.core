b15c16bb4de02afa2dcf0c2ceb60d9244cff46b8
DXCore Technology <i.love.to.smile.around@gmail.com>
2025-05-31 16:01:43 +0700

    Remove deprecated files and configurations from portal-app
    - Deleted unused Gulp configuration and related files.
    - Removed obsolete package-lock.json and package.json files.
    - Cleaned up various service and plugin files that are no longer in use.
    - Eliminated unnecessary CSS and JS files from the portal library.
    - Streamlined the project structure by removing outdated assets and dependencies.

--- .git-rewrite/t/src/portal-app/app/app-shared/services/file-service.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-app.factory('FileService', ['BaseService',
-    function (baseService) {
-
-        var serviceFactory = Object.create(baseService);
-        serviceFactory.init('file', true);        
-        return serviceFactory;
-
-    }]);
